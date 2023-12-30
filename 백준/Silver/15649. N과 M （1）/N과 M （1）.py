ffeaac5cf6e8d0abbdba833142a83856a3859343
kh277 <113894741+kh277@users.noreply.github.com>
2023-12-30 22:27:02 +0900

    [Silver III] Title: N과 M (1), Time: 212 ms, Memory: 36580 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,33 @@
+# 백준 15649번
+
+import sys
+input = sys.stdin.readline
+
+
+class DFS:
+    def __init__(self):
+        # 결과를 저장할 리스트
+        self.result = []
+
+    def recur(self, N: int, M: int, cur: list):
+        # 수열의 길이가 M에 도달할 경우 재귀 종료
+        if len(cur) == M:
+            self.result.append(cur)
+            return cur
+
+        # N, M, cur에 대한 정보를 가지고 재귀
+        for i in range(1, N + 1):
+            if i in cur:
+                continue
+            self.recur(N, M, cur + [i])
+
+
+def main():
+    N, M = map(int, input().split())
+    a = DFS()
+    a.recur(N, M, [])
+    for i in a.result:
+        print(*i)
+
+
+main()
