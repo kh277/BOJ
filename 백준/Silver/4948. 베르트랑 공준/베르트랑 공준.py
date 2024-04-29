0b95d9cd74ae71d34d4e31e4536e564d3d322437
kh277 <113894741+kh277@users.noreply.github.com>
2024-04-29 17:31:06 +0900

    [Silver II] Title: 베르트랑 공준, Time: 2760 ms, Memory: 110572 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -0,0 +1,38 @@
+# 백준 10971
+
+'''
+1 <= n <= 123,456이므로 O(N*squr(N))인 에라토스테네스의 체를 이용하자. 
+'''
+
+import sys
+import math
+
+input = sys.stdin.readline
+
+def check(num: int) -> bool:
+    for i in range(2, int(math.sqrt(num))+1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def solve(N: int) -> int:
+    result = 0
+
+    for i in range(N+1, 2*N+1):
+        if i == 1:
+            continue
+        if check(i):
+            result += 1
+
+    return result
+
+
+def main():
+    while True:
+        n = int(input())
+        if n == 0:
+            break
+        print(solve(n))
+
+main()
