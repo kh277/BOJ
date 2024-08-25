ddb3a12fda4042703291f8d3a403dde9cfeca83e
kh277 <113894741+kh277@users.noreply.github.com>
2024-08-25 20:26:32 +0900

    [Bronze III] Title: 공 넣기, Time: 100 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Bronze/10810. 공 넣기/공 넣기.py
@@ -0,0 +1,28 @@
+# 백준 10807
+
+import sys
+
+input = sys.stdin.readline
+
+
+def solve(N: int, ball: list) -> list:
+    DP = [0 for _ in range(N+1)]
+
+    for start, end, num in ball:
+        for i in range(start, end+1):
+            DP[i] = num
+
+    return DP[1:]
+
+
+def main():
+    N, M = map(int, input().split())
+
+    ball = []
+    for _ in range(M):
+        ball.append(list(map(int, input().split())))
+
+    print(*solve(N, ball))
+
+
+main()
