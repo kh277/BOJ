aae092db21a3d614bc04901ffe11229c0667e657
kh277 <113894741+kh277@users.noreply.github.com>
2024-11-27 14:37:32 +0900

    [Gold IV] Title: Zzz..., Time: 1276 ms, Memory: 197724 KB -BaekjoonHub

--- 백준/Gold/32771. Zzz．．．/Zzz．．．.py
+++ 백준/Gold/32771. Zzz．．．/Zzz．．．.py
@@ -2,7 +2,7 @@
 
 '''
 스위핑을 통해 각 방마다 예약된 시간대를 저장한 뒤, 구간을 뒤집어 예약이 없는 시간대를 저장하자.
-모든 방에 대해 예약이 없는 시간대를 스위핑을 통해 합치면 정답이 된다.
+그 뒤, 모든 방에 대해 예약이 없는 시간대를 스위핑을 통해 합치면 정답이 된다.
 '''
 
 import sys
@@ -11,25 +11,21 @@ input = sys.stdin.readline
 MAX_ROOM = 7
 
 
-# 스위핑 : limit개의 예약 L을 스위핑으로 중복 제거 처리
-def sweep(L, limit):
+# 스위핑 : 예약 시간이 저장된 리스트 L을 스위핑으로 중복 제거 처리
+def sweep(L):
+    length = len(L)
     L.sort(key= lambda x: (x[0], x[1]))
     result = []
-    start = L[0][0]
-    end = L[0][1]
-    index = 1
+    start, end = L[0]
 
-    while index < limit:
-        if end < L[index][0]:
+    for left, right in L[1:]:
+        if end < left:
             result.append([start, end])
-            start = L[index][0]
-            end = L[index][1]
+            start, end = left, right
         else:
-            end = max(end, L[index][1])
-        index += 1
-    
-    if limit > 0:
-        result.append([start, end])
+            end = max(end, right)
+
+    result.append([start, end])
     return result
 
 
@@ -37,11 +33,13 @@ def sweep(L, limit):
 def revSweep(L, limit):
     result = []
     start = 0
-    for i in range(len(L)):
-        result.append([start, L[i][0]])
-        start = L[i][1]
-    
-    result.append([start, limit])
+    for left, right in L:
+        result.append([start, left])
+        start = right
+
+    if start < limit:
+        result.append([start, limit])
+
     return result
 
 
@@ -49,15 +47,12 @@ def solve():
     interval = []
     # 스위핑으로 중복된 예약 시간 제거
     for i in range(MAX_ROOM):
-        reserv[i] = sweep(reserv[i], N[i])
-        interval += revSweep(reserv[i], openHour[i])
+        reserv[i] = sweep(reserv[i])
+        interval.extend(revSweep(reserv[i], openHour[i]))
     
     # 스위핑으로 각 방마다 빈 시간을 합쳐 누적 시간 반환
-    result = 0
-    for i in sweep(interval, len(interval)):
-        result += i[1] - i[0]
-    
-    return result
+    interval = sweep(interval)
+    return sum(right - left for left, right in interval)
 
 
 # main 함수 ----------
