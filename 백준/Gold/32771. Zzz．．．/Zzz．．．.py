4faac5ff0076aded13b7b272fd46d7f199854e01
kh277 <113894741+kh277@users.noreply.github.com>
2024-11-27 14:23:02 +0900

    [Gold IV] Title: Zzz..., Time: 1296 ms, Memory: 196140 KB -BaekjoonHub

--- 백준/Gold/32771. Zzz．．．/Zzz．．．.py
+++ 백준/Gold/32771. Zzz．．．/Zzz．．．.py
@@ -1,5 +1,10 @@
 # 백준 32771
 
+'''
+스위핑을 통해 각 방마다 예약된 시간대를 저장한 뒤, 구간을 뒤집어 예약이 없는 시간대를 저장하자.
+모든 방에 대해 예약이 없는 시간대를 스위핑을 통해 합치면 정답이 된다.
+'''
+
 import sys
 
 input = sys.stdin.readline
@@ -7,7 +12,7 @@ MAX_ROOM = 7
 
 
 # 스위핑 : limit개의 예약 L을 스위핑으로 중복 제거 처리
-def sweeping(L, limit):
+def sweep(L, limit):
     L.sort(key= lambda x: (x[0], x[1]))
     result = []
     start = L[0][0]
@@ -28,42 +33,30 @@ def sweeping(L, limit):
     return result
 
 
+# 구간 뒤집기 : 현재 방의 영업 시간(0 ~ limit)에 대해 예약이 없는 시간만 도출해 반환
+def revSweep(L, limit):
+    result = []
+    start = 0
+    for i in range(len(L)):
+        result.append([start, L[i][0]])
+        start = L[i][1]
+    
+    result.append([start, limit])
+    return result
+
+
 def solve():
-    index = [0 for _ in range(MAX_ROOM)]        # 현재까지 처리한 예약의 인덱스 저장
-    time = sorted(list(timeSet))        # 예약된 시간대를 정렬해서 저장
-
-    # curTime ~ nextTime동안 사용 가능한지 확인
+    interval = []
+    # 스위핑으로 중복된 예약 시간 제거
+    for i in range(MAX_ROOM):
+        reserv[i] = sweep(reserv[i], N[i])
+        interval += revSweep(reserv[i], openHour[i])
+    
+    # 스위핑으로 각 방마다 빈 시간을 합쳐 누적 시간 반환
     result = 0
-    for i in range(1, len(time)):
-        curTime = time[i-1]
-        nextTime = time[i]
-        addTime = 0
-
-        # 모든 방에 대해 탐색
-        for j in range(MAX_ROOM):
-            # 모든 예약이 다 처리된 경우
-            if index[j] == len(reserv[j]):
-                if curTime >= openHour[j]:      # 영업 시간이 끝난 경우
-                    continue
-                else:       # 예약이 다 처리되었고, 영업 시간이 끝나지 않은 경우
-                    addTime = nextTime - curTime
-                    continue
-
-            # nextTime에 예약이 끝나는 경우 -> index 증가
-            elif nextTime == reserv[j][index[j]][1]:
-                index[j] += 1
-                continue
-
-            # 잠방이 사용중인 경우
-            elif curTime >= reserv[j][index[j]][0] and nextTime <= reserv[j][index[j]][1]:
-                continue
-
-            # 잠방이 빈 경우
-            else:
-                addTime = nextTime - curTime
-                continue
-        result += addTime
-
+    for i in sweep(interval, len(interval)):
+        result += i[1] - i[0]
+    
     return result
 
 
@@ -71,20 +64,10 @@ def solve():
 N = list(map(int, input().split()))
 openHour = list(map(int, input().split()))
 reserv = [[] for _ in range(MAX_ROOM)]
-timeSet = set()     # 예약 시작 시간, 예약 끝 시간 및 방 마감 시간 전부 저장
-timeSet.add(0)
 
 for i in range(MAX_ROOM):
     for j in range(N[i]):
         s, e = map(int, input().split())
         reserv[i].append([s, e])
-        timeSet.add(s)
-        timeSet.add(e)
-for i in openHour:
-    timeSet.add(i)
-
-# 스위핑으로 중복된 예약 시간 제거
-for i in range(MAX_ROOM):
-    reserv[i] = sweeping(reserv[i], N[i])
 
 print(solve())
