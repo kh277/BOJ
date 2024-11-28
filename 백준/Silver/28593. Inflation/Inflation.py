f917bab6b7aa3d10d3770f2dddf938da5415aecb
kh277 <113894741+kh277@users.noreply.github.com>
2024-11-28 11:21:46 +0900

    [Silver I] Title: Inflation, Time: 268 ms, Memory: 182264 KB, Score: 70 point -BaekjoonHub

---
+++ 백준/Silver/28593. Inflation/Inflation.py
@@ -0,0 +1,53 @@
+# 백준 28593
+
+'''
+누적 합을 저장하자. 또한, 수의 범위가 1~100만이므로
+크기가 200만인 리스트 data를 생성하고, startPoint를 100만으로 둔다.
+이 data 리스트에는 해당 숫자가 몇 개 있는지를 저장한다.
+
+모든 수를 더하는 쿼리의 경우 startPoint를 그 수만큼 감소시키면 되고,
+일정 수를 고치는 쿼리의 경우 data에서 startPoint + 그 수에 해당하는 인덱스의 값만큼 수정해주면 된다.
+'''
+
+import sys
+
+input = sys.stdin.readline
+
+
+def add(accSum, i, startPoint):
+    startPoint -= i
+    accSum += N*int(i)
+
+    return accSum, startPoint
+
+
+def update(accSum, i, j, startPoint):
+    accSum += data[startPoint+i]*(j-i)
+    if i != j:
+        data[startPoint+j] += data[startPoint+i]
+        data[startPoint+i] = 0
+
+    return accSum
+
+
+# main 함수 ----------
+N = int(input())
+P = list(map(int, input().split()))
+
+# 데이터 초기 처리
+data = [0 for _ in range(2000001)]
+startPoint = 1000000
+accSum = sum(P)
+for i in P:
+    data[startPoint+i] += 1
+
+# 쿼리 처리
+Q = int(input())
+for _ in range(Q):
+    query = list(map(str, input().split()))
+    if query[0] == 'INFLATION':
+        accSum, startPoint = add(accSum, int(query[1]), startPoint)
+    else:
+        accSum = update(accSum, int(query[1]), int(query[2]), startPoint)
+
+    print(accSum)
