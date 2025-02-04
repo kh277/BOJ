90ce109cb3695dd321873a50963bcc3c17a5c745
kh277 <113894741+kh277@users.noreply.github.com>
2025-02-04 23:48:16 +0900

    [Silver V] Title: 돌 게임, Time: 100 ms, Memory: 108608 KB -BaekjoonHub

---
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -0,0 +1,21 @@
+# 백준 9655
+
+'''
+상대가 돌을 넘길 때 돌의 개수가 2의 배수가 되면 무조건 이기게 된다.
+'''
+
+import io
+
+input = io.BufferedReader(io.FileIO(0), 1<<18).readline
+
+
+def solve():
+    if N % 2 == 1:
+        return 'SK'
+    else:
+        return 'CY'
+
+
+# main 함수 ----------
+N = int(input())
+print(solve())
