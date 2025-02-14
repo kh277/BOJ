468510bec3066e4784e768914580faaebe9047c4
kh277 <113894741+kh277@users.noreply.github.com>
2025-02-15 02:28:25 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 996 ms, Memory: 116116 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -8,39 +8,43 @@ print = os.write
 CHUNK = 4096
 
 
-# main 함수 ----------
-N = int(input())
-count = [0] * 10001
+def main():
+    N = int(input())
+    count = [0] * 10001
 
-for _ in range(N):
-    count[int(input())] += 1
+    for _ in range(N):
+        count[int(input())] += 1
 
-buf = bytearray()
-lines = [None] * 10001
-for num in range(1, 10001):
-    cur = count[num]
-    if not cur:
-        continue
-    if lines[num] is None:
-        lines[num] = (str(num) + "\n").encode("ascii")
+    buf = bytearray()
+    lines = [None] * 10001
+    for num in range(1, 10001):
+        cur = count[num]
+        if not cur:
+            continue
 
-    line = lines[num]
-    if cur < CHUNK:
-        buf.extend(line * cur)
-    else:
-        if buf:
+        if lines[num] is None:
+            lines[num] = (str(num) + "\n").encode("ascii")
+
+        line = lines[num]
+        if cur < 4096:
+            buf.extend(line * cur)
+        else:
+            if buf:
+                print(1, buf)
+                buf = bytearray()
+            while cur:
+                k = cur if cur < 4096 else 4096
+                print(1, line * k)
+                cur -= k
+        
+        if len(buf) > 65536:
             print(1, buf)
             buf = bytearray()
-        while cur:
-            k = cur if cur < CHUNK else CHUNK
-            print(1, line * k)
-            cur -= k
-    
-    if len(buf) > 65536:
+
+    if buf:
         print(1, buf)
-        buf = bytearray()
 
-if buf:
-    print(1, buf)
+    os._exit(0)
 
-os._exit(0)
+
+main()
