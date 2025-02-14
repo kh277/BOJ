40561098dd04666c31cdbad8f67786c55a6faf9d
kh277 <113894741+kh277@users.noreply.github.com>
2025-02-15 02:10:28 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1004 ms, Memory: 116032 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,26 +1,46 @@
 # 백준 10989
 
 import io
-import sys
+import os
 
 input = io.BufferedReader(io.FileIO(0), 1<<18).readline
-print = sys.stdout.write
+print = os.write
 CHUNK = 4096
 
 
 # main 함수 ----------
 N = int(input())
-count = [0 for _ in range(10001)]
+count = [0] * 10001
 
 for _ in range(N):
     count[int(input())] += 1
 
-result = []
-for i in range(1, 10001):
-    cur = count[i]
-    if cur:
-        s = str(i) + "\n"
+buf = bytearray()
+lines = [None] * 10001
+for num in range(1, 10001):
+    cur = count[num]
+    if not cur:
+        continue
+    if lines[num] is None:
+        lines[num] = (str(num) + "\n").encode("ascii")
+
+    line = lines[num]
+    if cur < CHUNK:
+        buf.extend(line * cur)
+    else:
+        if buf:
+            print(1, buf)
+            buf = bytearray()
         while cur:
             k = cur if cur < CHUNK else CHUNK
-            print(s*k)
+            print(1, line * k)
             cur -= k
+    
+    if len(buf) > 65536:
+        print(1, buf)
+        buf = bytearray()
+
+if buf:
+    print(1, buf)
+
+os._exit(0)
