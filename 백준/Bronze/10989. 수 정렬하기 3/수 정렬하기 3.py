4f31aa1c760fd6a057cba2dc2cd0791a39149284
kh277 <113894741+kh277@users.noreply.github.com>
2025-02-15 01:33:25 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1140 ms, Memory: 111104 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,19 +1,26 @@
 # 백준 10989
 
 import io
+import sys
 
 input = io.BufferedReader(io.FileIO(0), 1<<18).readline
+print = sys.stdout.write
+CHUNK = 4096
 
 
 # main 함수 ----------
 N = int(input())
-num = [0 for _ in range(10001)]
-for _ in range(N):
-    num[int(input())] += 1
+count = [0 for _ in range(10001)]
 
+for _ in range(N):
+    count[int(input())] += 1
+
+result = []
 for i in range(1, 10001):
-    if num[i] == 0:
-        continue
-    else:
-        for j in range(num[i]):
-            print(i)
+    cur = count[i]
+    if cur:
+        s = str(i) + "\n"
+        while cur:
+            k = cur if cur < CHUNK else CHUNK
+            print(s*k)
+            cur -= k
