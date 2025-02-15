18d0d370e3da593dc9972e6c0dd8cbb7e0d17ad4
kh277 <113894741+kh277@users.noreply.github.com>
2025-02-15 20:12:01 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 2776 ms, Memory: 347488 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
@@ -5,39 +5,50 @@
 자신이 EA일 경우는 자신의 자식 노드들은 EA일 수도 있고 아닐 수도 있다. 이 경우, 최소 EA값을 저장하면 된다.
 모든 정점은 얼리 어답터이거나, 일반 정점이거나 둘 중 하나 뿐이다.
 DP[i][type] = i번째 정점이 type(일반 정점=0, 얼리 어답터=1)일 경우, 자기 노드 + 모든 자손 노드 중 최소 얼리어답터 수
+
+pypy3 언어로 재귀 탐색을 할 경우 메모리 초과가 발생하게 된다.
+따라서 트리를 후위 순회 순서로 DP 처리를 하자.
 '''
 
 import io
-import sys
-import random
 
-sys.setrecursionlimit(10**6)
 input = io.BufferedReader(io.FileIO(0), 1<<18).readline
 
 
-def recur(curNode, graph, DP, visited):
-    visited[curNode] = True
+def solve(N, graph):
+    DP = [[0, 0] for _ in range(N+1)]
+    parent = [0] * (N+1)
+    postorder = []      # 후위 순회 순서 저장
+    startPos = 1
 
-    # 자신이 EA인 경우 처리
-    DP[curNode][1] += 1
+    # 후위 순회 순서 구하기
+    stack = [1]
+    parent[startPos] = -1
+    while stack:
+        curNode = stack.pop()
+        postorder.append(curNode)
 
-    for nextNode in graph[curNode]:
-        if visited[nextNode] == False:
-            recur(nextNode, graph, DP, visited)
-            
+        for nextNode in graph[curNode]:
+            if nextNode == parent[curNode]:
+                continue
+
+            parent[nextNode] = curNode
+            stack.append(nextNode)
+
+    # 후위 순회의 역순으로 DP 탐색
+    for curNode in reversed(postorder):
+        DP[curNode][1] += 1     # 자신이 EA인 경우 처리
+
+        for nextNode in graph[curNode]:
+            if nextNode == parent[curNode]:
+                continue
+
             # 자신이 EA가 아닌 경우 -> 자식이 반드시 EA여야 함
             DP[curNode][0] += DP[nextNode][1]
 
             # 자신이 EA인 경우
             DP[curNode][1] += min(DP[nextNode])
 
-
-def solve(N, graph):
-    DP = [[0, 0] for _ in range(N+1)]
-    visited = [False for _ in range(N+1)]
-    startPos = random.randrange(1, N+1)
-
-    recur(startPos, graph, DP, visited)
     return min(DP[startPos])
 
 
