5844c076cebf9af0a9a59cdb23b0e40cd89bfac2
kh277 <113894741+kh277@users.noreply.github.com>
2025-02-19 17:28:59 +0900

    [Platinum IV] Title: 열혈강호, Time: 616 ms, Memory: 252676 KB -BaekjoonHub

--- 백준/Platinum/11375. 열혈강호/열혈강호.py
+++ 백준/Platinum/11375. 열혈강호/열혈강호.py
@@ -1,51 +1,33 @@
 # 백준 11375
 
-import sys
+import io, sys
 
-input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
+input = io.BufferedReader(io.FileIO(0), 1<<18).readline
+sys.setrecursionlimit(10**5)
 
 
-def DFS(data: list, cur_worker: int, complete: list, assignA: list, assignB: list) -> bool:
-    complete[cur_worker] = True
+# B에서 curV과 연결될 수 있는지 여부 체크
+def DFS(curV, graph, visited, matchB):
+    for nextV in graph[curV]:
+        if visited[nextV] == True:
+            continue
+        visited[nextV] = True
 
-    # 현재 작업자가 할 수 있는 일에 대해 반복
-    for work in data[cur_worker]:
-        # 해당 일이 할당되지 않은 경우 할당
-        if assignB[work] == 0:
-            assignA[cur_worker] = work
-            assignB[work] = cur_worker
-
+        # nextV가 매칭되지 않았거나, 다른 정점과 매칭시킬 수 있다면 -> 연결
+        if matchB[nextV] == 0 or DFS(matchB[nextV], graph, visited, matchB):
+            matchB[nextV] = curV
             return True
-            
-        # 해당 일이 다른 작업자에게 할당되어 있지만, 그 작업자가 다른 일을 할 수 있는 경우
-        elif not complete[assignB[work]] and DFS(data, assignB[work], complete, assignA, assignB):
-            assignA[cur_worker] = work
-            assignB[work] = cur_worker
 
-            return True
-        
     return False
-    
 
-def match(worker: int, work: int, data: list) -> int:
-    # 각 작업에 할당된 작업자의 번호 저장 (-1은 미할당)
-    # assignA[i]는 A그룹의 i번째 작업자와 매칭된 B그룹의 작업 번호 저장
-    # assignB[i]는 B그룹의 i번째 작업 번호와 매칭된 A그룹의 작업자 저장
-    assignA = [0 for _ in range(worker+1)]
-    assignB = [0 for _ in range(work+1)]
 
+def Ford_Fulkerson(numA, numB, graph):
+    matchB = [0 for _ in range(numB+1)]
     result = 0
 
-    # 모든 작업자에 대해 반복
-    for cur_worker in range(1, worker+1):
-        # 아직 매칭되지 않은 경우
-        if assignA[cur_worker] == 0:
-            complete = [False for _ in range(worker+1)]
-
-            # 현재 작업자에 대해 매칭 시도
-            if DFS(data, cur_worker, complete, assignA, assignB):
-                result += 1
+    for curV in range(1, numA+1):
+        if DFS(curV, graph, [False for _ in range(numB+1)], matchB):
+            result += 1
 
     return result
 
@@ -53,11 +35,12 @@ def match(worker: int, work: int, data: list) -> int:
 def main():
     N, M = map(int, input().split())
 
-    data = [[] for _ in range(N+1)]
+    # 간선 정보 전처리
+    graph = [[] for _ in range(N+1)]
     for i in range(1, N+1):
-        data[i] = list(map(int, input().split()))[1:]
+        graph[i] = list(map(int, input().split()))[1:]
 
-    print(match(N, M, data))
+    print(Ford_Fulkerson(N, M, graph))
 
 
 main()
