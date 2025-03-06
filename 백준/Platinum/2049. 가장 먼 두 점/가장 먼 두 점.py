b80c6ac2e066cdd47f8e2e31dea94bd9752a4bbd
kh277 <113894741+kh277@users.noreply.github.com>
2025-03-06 15:49:59 +0900

    [Platinum III] Title: 가장 먼 두 점, Time: 396 ms, Memory: 138936 KB -BaekjoonHub

--- 백준/Platinum/2049. 가장 먼 두 점/가장 먼 두 점.py
+++ 백준/Platinum/2049. 가장 먼 두 점/가장 먼 두 점.py
@@ -4,82 +4,86 @@
 볼록 껍질을 구성하는 점을 구하고 회전하는 캘리퍼스를 이용해 두 점 사이의 거리의 최대값을 구한다.
 '''
 
+import io
 
-import sys
-
-input = sys.stdin.readline
-
-
-def cross(a, b, c):
-    # 양수(반시계), 0(일직선), 음수(시계)
-    return (b[0] - a[0]) * (c[1] - a[1]) - (b[1] - a[1]) * (c[0] - a[0])
+input = io.BufferedReader(io.FileIO(0), 1<<18).readline
 
 
-def distance(a: list, b: list) -> int:
-    return (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2
+def CCW(A, B, C):
+    return (B[0]-A[0]) * (C[1]-A[1]) - (B[1]-A[1]) * (C[0]-A[0])
 
 
-def convex_hull(graph):
+def distance(A, B):
+    return (A[0]-B[0])**2 + (A[1]-B[1])**2
 
-    # 구성하는 점이 3개 미만일 경우
-    if len(graph) < 3:
-        return graph
 
+def ConvexHull(points):
     # x좌표 오름차순으로 정렬
-    graph = sorted(set(graph))
+    points = sorted(set(points))
 
-    # 아래쪽 Hull을 구함
+    # 아래쪽 Hull 구하기
     lower = []
-    for i in graph:
+    for point in points:
         # 반시계 방향이 아닐 경우 마지막 점 제거
-        while len(lower) >= 2 and cross(lower[-2], lower[-1], i) <= 0:
+        while len(lower) >= 2 and CCW(lower[-2], lower[-1], point) <= 0:
             lower.pop()
-        lower.append(i)
-        
-    # 위쪽 Hull을 구함
+        lower.append(point)
+
+    # 위쪽 Hull 구하기
     upper = []
-    for i in reversed(graph):
+    for point in reversed(points):
         # 반시계 방향이 아닐 경우 마지막 점 제거
-        while len(upper) >= 2 and cross(upper[-2], upper[-1], i) <= 0:
+        while len(upper) >= 2 and CCW(upper[-2], upper[-1], point) <= 0:
             upper.pop()
-        upper.append(i)
-    
+        upper.append(point)
+
     # 아래쪽 Hull과 위쪽 Hull을 중복제거하여 합치기
     return lower[:-1] + upper[:-1]
 
 
-def rotating_calipers(graph: list) -> int:
-    hull = convex_hull(graph)
+def RotatingCalipers(points):
+    # 볼록 껍질 구하기
+    hull = ConvexHull(points)
     length = len(hull)
 
-    # 점이 2개 미만인 경우
+    # 특수 케이스 처리
     if length < 2:
         return 0
-
-    # 점이 2개인 경우
-    if length == 2:
+    elif length == 2:
         return distance(hull[0], hull[1])
     
-    max_distance = 0
-    
-    # hull의 모든 점들에 대해 비교하여 최장 거리 도출
-    for i in range(length):
-        for j in range(i+1, length):
-            max_distance = max(max_distance, distance(hull[i], hull[j]))
-    
-    # 최대 거리 반환
-    return max_distance
+    result = 0
+    curFarestP = 1
+    for curP in range(length):
+        nextP = (curP+1) % length
+
+        # i와 다음 점 next_i을 잇는 직선과 가장 멀리 떨어진 점 도출
+        while True:
+            nextFarestP = (curFarestP+1) % length
+
+            # 두 점 curP, nextP에서 curFarestP, nextFarestP까지 거리 비교
+            d1 = CCW(hull[curP], hull[nextP], hull[curFarestP])
+            d2 = CCW(hull[curP], hull[nextP], hull[nextFarestP])
+
+            if d1 < d2:
+                curFarestP = nextFarestP
+            else:
+                break
+
+        # 갱신
+        result = max(result, distance(hull[curP], hull[curFarestP]), distance(hull[nextP], hull[curFarestP]))
+
+    return result
 
 
 def main():
-    n = int(input())
+    N = int(input())
     graph = []
-    
-    for i in range(n):
+    for _ in range(N):
         graph.append(tuple(map(int, input().split())))
     
     # 중복 점 제거
-    print(rotating_calipers(list(set(graph))))
+    print(RotatingCalipers(list(set(graph))))
 
 
 main()
