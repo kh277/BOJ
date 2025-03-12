09921ba81d4ad08f13258bd0b3434eebee2faa01
kh277 <113894741+kh277@users.noreply.github.com>
2025-03-13 00:16:23 +0900

    [Gold I] Title: 외판원 순회, Time: 224 ms, Memory: 119216 KB -BaekjoonHub

--- 백준/Gold/2098. 외판원 순회/외판원 순회.py
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.py
@@ -1,74 +1,48 @@
 # 백준 2098
 
 '''
-외판원 문제이지만, 2 <= N <= 10이므로 브루트포스로도 풀린다.
-1. 방문하지 않은 next 도시들에 대해 here -> next -> start 로 돌아오는 비용을 계산한다.
-2. 재귀를 통해 방문 도시를 늘려가며 반복한다.
-3. 모든 도시를 방문할 때까지 1~2번을 반복한다.
-
-주의 반례 :
-4
-0 1 0 0
-0 0 1 0
-0 0 0 1
-1 0 0 0
+방문 처리를 비트마스킹으로 하고, 재귀 DP를 통해 정점 순회를 하면 된다.
 '''
 
-import sys
+import io
 
-input = sys.stdin.readline
-
-
-# next를 방문했는지 확인
-def check_visit(status: int, city: int) -> bool:
-    if status & (1 << city):
-        return True
-    return False
+input = io.BufferedReader(io.FileIO(0), 1<<18).readline
+INF = 10**8
+START = 0
 
 
-def TSP(N: int, graph: list, status: int, here: int, DP: list) -> int:
-    # 도시를 다 방문한 경우
-    # 경로가 존재한다면 here -> start로 가는 값 반환
-    # 경로가 존재하지 않는다면 매우 큰 값 반환
+def TSP(N, graph, status, curV, DP):
+    # 도시를 다 방문했고, START로의 경로가 존재한다면 curV -> START로 가는 값 반환
     if status == (1 << N) - 1:
-        return graph[here][0] if graph[here][0] != 0 else 10e8
-    
-    # 이전에 계산한 값이면 그 값 반환
-    if not DP[here][status] == -1:
-        return DP[here][status]
+        return graph[curV][START] if graph[curV][START] != 0 else INF
 
-    DP[here][status] = 10e8
+    # 이전에 계산한 값일 경우
+    if not DP[curV][status] == 0:
+        return DP[curV][status]
+
+    DP[curV][status] = INF
 
     # 방문하지 않은 도시들에 대해 반복
-    for next in range(0, N):
-        if not check_visit(status, next) and not graph[here][next] == 0:
-            # (here에서 next로 가는 비용) + (next에서 나머지 도시를 거쳐 start로 가는 비용) 계산
-            # status에서 next 위치를 0으로 변경
-            temp = graph[here][next] + TSP(N, graph, status | (1 << next), next, DP)
+    for nextV in range(N):
+        if (status & (1 << nextV)) == 0 and graph[curV][nextV] != 0:
+            # (curV에서 nextV 가는 비용) + (nextV에서 나머지 도시를 거쳐 START로 가는 비용) 계산 및 nextV 방문처리
+            temp = graph[curV][nextV] + TSP(N, graph, status | (1 << nextV), nextV, DP)
 
             # 최소값 갱신이 가능하다면 갱신
-            if temp < DP[here][status]:
-                DP[here][status] = temp
+            if temp < DP[curV][status]:
+                DP[curV][status] = temp
     
-    return DP[here][status]
-
-
-def solve(N: int, graph: list) -> int:
-    DP = [[-1 for _ in range(2**N)] for _ in range(N)]
-    
-    # 순회 문제이기 때문에 시작 지점은 어디를 잡든지 상관이 없음
-    # 이 코드에서는 시작점을 0으로 잡았음
-    return TSP(N, graph, 1, 0, DP)
+    return DP[curV][status]
 
 
 def main():
     N = int(input())
     graph = []
 
-    for i in range(N):
+    for _ in range(N):
         graph.append(list(map(int, input().split())))
     
-    print(solve(N, graph))
+    print(TSP(N, graph, 1<<START, 0, [[0 for _ in range(2**N)] for _ in range(N)]))
 
 
 main()
