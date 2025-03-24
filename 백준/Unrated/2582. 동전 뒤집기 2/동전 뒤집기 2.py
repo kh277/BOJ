674e2fe3e82ed8206f5cdd20a91e2848d898c556
kh277 <113894741+kh277@users.noreply.github.com>
2025-03-24 13:47:31 +0900

    [Unrated] Title: 동전 뒤집기 2, Time: 4008 ms, Memory: 206568 KB, Score: 9 point -BaekjoonHub

---
+++ 백준/Unrated/2582. 동전 뒤집기 2/동전 뒤집기 2.py
@@ -0,0 +1,74 @@
+# 백준 2582
+
+import io
+from random import random
+from copy import deepcopy
+from math import exp
+
+input = io.BufferedReader(io.FileIO(0), 1<<18).readline
+DECREASE_RATE = 0.9999      # 감쇄율
+BOLTZMANN_CONSTANT = 10     # 볼츠만 상수
+MAX_EXPONENT = 700          # e^x를 계산하기 위한 값의 상한치
+curTemperature = 1
+limitTemperature = 0.001
+
+
+
+def scoring(coin):
+    return sum(sum(x) for x in coin)
+
+
+def move(N, coin):
+    # revCol열 뒤집기
+    revCol = int(random()*1000) % N
+    for y in range(N):
+        coin[y][revCol] ^= 1
+    
+    # 나머지 행에서 1이 과반수를 넘으면 뒤집기
+    for y in range(N):
+        if sum(coin[y])*2 > N:
+            for x in range(N):
+                coin[y][x] ^= 1
+    
+    return coin
+
+
+def SimilatedAnnealing(N, coin):
+    global curTemperature
+
+    result = 1025
+    while curTemperature > limitTemperature:
+        curState = deepcopy(coin)
+
+        # cur -> next 상태로 전이
+        coin = move(N, coin)
+
+        # 점수 계산
+        curEnergy = scoring(curState)
+        nextEnergy = scoring(coin)
+
+        probability = exp(min(MAX_EXPONENT, (nextEnergy - curEnergy) / (BOLTZMANN_CONSTANT * curTemperature)))
+
+        # 랜덤한 확률로 상태 전이
+        if (probability > random()):
+            coin = curState
+
+        # 온도 감률 적용
+        curTemperature *= DECREASE_RATE
+        result = min(result, scoring(coin))
+
+    return result
+
+
+def main():
+    N = int(input())
+    coin = []
+    for _ in range(N):
+        coin.append(list(map(str, input().decode().rstrip())))
+    for y in range(N):
+        for x in range(N):
+            coin[y][x] = 1 if coin[y][x] == 'T' else 0
+    print(SimilatedAnnealing(N, coin))
+
+
+main()
