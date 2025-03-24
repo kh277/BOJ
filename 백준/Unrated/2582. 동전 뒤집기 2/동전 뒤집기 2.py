ca308f79c6f3713839040dae76a1b5c6574e351c
kh277 <113894741+kh277@users.noreply.github.com>
2025-03-24 15:29:43 +0900

    [Unrated] Title: 동전 뒤집기 2, Time: 852 ms, Memory: 115736 KB, Score: 29 point -BaekjoonHub

--- 백준/Unrated/2582. 동전 뒤집기 2/동전 뒤집기 2.py
+++ 백준/Unrated/2582. 동전 뒤집기 2/동전 뒤집기 2.py
@@ -2,34 +2,34 @@
 
 import io
 import random
-from copy import deepcopy
 from math import exp
+from array import array
 
 input = io.BufferedReader(io.FileIO(0), 1<<18).readline
-DECREASE_RATE = 0.9998      # 감쇄율
+
+DECREASE_RATE = 0.9999      # 감쇄율
 BOLTZMANN_CONSTANT = 10     # 볼츠만 상수
 MAX_EXPONENT = 700          # e^x를 계산하기 위한 값의 상한치
-curTemperature = 1
-limitTemperature = 0.0005
-
+curTemperature = 1          # 시작 온도
+limitTemperature = 0.0005   # 임계 온도
 
 
 def scoring(coin):
-    return sum(sum(x) for x in coin)
+    return sum(coin)
 
 
 def move(N, coin):
-    # revCol열 뒤집기
+    # 랜덤하게 세로줄 뒤집기
     revCol = int(random.randint(0, 32768)) % N
     for y in range(N):
-        coin[y][revCol] ^= 1
+        coin[y*N + revCol] ^= 1
     
-    # 나머지 행에서 1이 과반수를 넘으면 뒤집기
+    # 1이 과반수를 넘으면 가로줄이 있으면 뒤집기
     for y in range(N):
-        if sum(coin[y])*2 > N:
-            for x in range(N):
-                coin[y][x] ^= 1
-    
+        curRow = coin[y*N:(y+1)*N]
+        if sum(curRow)*2 > N:
+            coin[y*N:(y+1)*N] = array('I', [x^1 for x in curRow])
+
     return coin
 
 
@@ -37,23 +37,20 @@ def SimilatedAnnealing(N, coin):
     global curTemperature
 
     result = 1025
+    # 임계 온도에 도달할 때까지 반복
     while curTemperature > limitTemperature:
-        curState = deepcopy(coin)
+        curState = coin[:]
 
-        # cur -> next 상태로 전이
-        coin = move(N, coin)
-
-        # 에너지 계산
+        # 다음 상태로 전이 및 에너지 계산
+        nextState = move(N, coin)
         curEnergy = scoring(curState)
-        nextEnergy = scoring(coin)
+        nextEnergy = scoring(nextState)
 
-        # 전이 후 에너지가 낮을 경우 확률적으로 상태 전이
-        if (nextEnergy - curEnergy) < 0:
-            probability = exp(min(MAX_EXPONENT, (nextEnergy - curEnergy) / (BOLTZMANN_CONSTANT * curTemperature)))
-
-            # 랜덤한 확률로 상태 전이
-            if (probability > random.random()):
-                coin = curState
+        # 랜덤한 확률로 상태 전이
+        # 에너지가 커진다면 거의 무조건 전이, 에너지가 작아진다면 확률적으로 전이
+        probability = exp(min(MAX_EXPONENT, (nextEnergy - curEnergy) / (BOLTZMANN_CONSTANT * curTemperature)))
+        if (probability >= random.random()):
+            coin = nextState
 
         # 온도 감률 적용
         curTemperature *= DECREASE_RATE
@@ -64,12 +61,12 @@ def SimilatedAnnealing(N, coin):
 
 def main():
     N = int(input())
-    coin = []
+    coin = array('I')
     for _ in range(N):
-        coin.append(list(map(str, input().decode().rstrip())))
-    for y in range(N):
-        for x in range(N):
-            coin[y][x] = 1 if coin[y][x] == 'T' else 0
+        string = input().decode().rstrip()
+        for ch in string:
+            coin.append(1 if ch == 'T' else 0)
+
     print(SimilatedAnnealing(N, coin))
 
 
