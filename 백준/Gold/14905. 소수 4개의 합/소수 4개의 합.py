4d8354be511675a386170e2166c705cc3afbf936
kh277 <113894741+kh277@users.noreply.github.com>
2025-04-08 21:34:01 +0900

    [Gold III] Title: 소수 4개의 합, Time: 88 ms, Memory: 108608 KB -BaekjoonHub

---
+++ 백준/Gold/14905. 소수 4개의 합/소수 4개의 합.py
@@ -0,0 +1,51 @@
+# 백준 14905
+
+import io
+
+input = io.BufferedReader(io.FileIO(0), 1<<18).readline
+
+
+# 소수 판별 함수
+def checkPrime(N):
+    if N <= 1:
+        return False
+    elif N <= 3:
+        return True
+    elif N % 2 == 0 or N % 3 == 0:
+        return False
+
+    for i in range(5, int(N**0.5)+1, 6):
+        if N % i == 0 or N % (i + 2) == 0:
+            return False
+
+    return True
+
+
+def backtrack(N, num):
+    # 백트래킹 종료 조건
+    if len(num) == 4:
+        return num
+
+    # N 이하에 대해 백트래킹
+    for i in range(N, 1, -1):
+        if checkPrime(i) == True:
+            temp = backtrack(N-i, num + [i])
+            if temp == [-1]:
+                continue
+            return temp
+
+    return [-1]
+
+
+def solve(N):
+    if N < 8:
+        return ['Impossible.']
+    return backtrack(N, [])
+
+
+while True:
+    try:
+        N = int(input())
+        print(*solve(N))
+    except:
+        break
