180d85e5facd1d9a2faf1cdea9c55e27e297093e
kh277 <113894741+kh277@users.noreply.github.com>
2025-04-11 01:14:42 +0900

    [Gold IV] Title: 두 배, Time: 196 ms, Memory: 146052 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31963. 두 배/두 배.py
+++ 백준/Gold/31963. 두 배/두 배.py
@@ -1,34 +1,32 @@
 # 백준 31963
 
+'''
+num[i] * 2^k <= num[i+1] * 2^x인 x를 구하면 된다.
+이전 수의 k값을 저장하는 변수를 이용하여 x = ceil(log2(num[i]/num[i+1])) + k로 구한다.
+'''
+
 import io
+import math
 
 input = io.BufferedReader(io.FileIO(0), 1<<18).readline
 
 
-# a * 2^x >= b의 해 구하기
-def exp(a, b):
-    count = 0
-    while True:
-        if a >= b:
-            return [count, a]
-        a *= 2
-        count += 1
-
-
 def solve(N, num):
     count = 0
-    foreNum = num[0]
-    index = 0
+    foreK = 0
+    index = 1
     while index < N:
-        curNum = num[index]
         # 오름차순이 아닐 경우
-        if foreNum > curNum:
-            c, curNum = exp(curNum, foreNum)
-            count += c
+        if foreK > math.log2(num[index]/num[index-1]):
+            curK = math.ceil(math.log2(num[index-1]/num[index])) + foreK
+            foreK = curK
+            count += curK
+        # 오름차순일 경우
+        else:
+            foreK = 0
 
-        foreNum = curNum
         index += 1
-    
+
     return count
 
 
