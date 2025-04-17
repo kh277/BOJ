f4021236d0023ac9b8c4cc60d16b759537acb2c9
kh277 <113894741+kh277@users.noreply.github.com>
2025-04-17 14:35:56 +0900

    [Silver IV] Title: 제로, Time: 104 ms, Memory: 112088 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.py
+++ 백준/Silver/10773. 제로/제로.py
@@ -3,19 +3,21 @@
 import io
 from array import array
 
-imput = io.BufferedReader(io.FileIO(0), 1<<18).readline
+input = io.BufferedReader(io.FileIO(0), 1<<18).readline
 
 def main():
     K = int(input())
     stack = array('I')
+    accSum = 0
     for _ in range(K):
         N = int(input())
         if N == 0:
-            stack.pop()
+            accSum -= stack.pop()
         else:
             stack.append(N)
+            accSum += N
     
-    print(sum(stack))
+    print(accSum)
 
 
 main()
