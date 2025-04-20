b382b7a076d6e925bb6dfa1f287f8d448e84b92b
kh277 <113894741+kh277@users.noreply.github.com>
2025-04-21 01:11:53 +0900

    [Gold V] Title: 강의실 배정, Time: 412 ms, Memory: 157080 KB -BaekjoonHub

---
+++ 백준/Gold/11000. 강의실 배정/강의실 배정.py
@@ -0,0 +1,33 @@
+# 백준 11000
+
+import io
+import heapq
+
+input = io.BufferedReader(io.FileIO(0), 1<<18).readline
+
+
+def solve(N, data):
+    data.sort(key= lambda x: (x[0], x[1]))
+
+    pq = [0]
+    for i in range(N):
+        curS, curE = data[i]
+        leastRoom = heapq.heappop(pq)
+        if curS >= leastRoom:
+            heapq.heappush(pq, curE)
+        else:
+            heapq.heappush(pq, leastRoom)
+            heapq.heappush(pq, curE)
+        
+    return len(pq)
+
+
+def main():
+    N = int(input())
+    data = []
+    for _ in range(N):
+        data.append(list(map(int, input().split())))
+    print(solve(N, data))
+
+
+main()
