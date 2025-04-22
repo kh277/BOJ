72d0227dd0baa8ccc6f90683fed2b3e5c7338845
kh277 <113894741+kh277@users.noreply.github.com>
2025-04-22 10:07:04 +0900

    [Platinum IV] Title: 챔피언소트 (Small), Time: 88 ms, Memory: 108608 KB -BaekjoonHub

--- 백준/Platinum/12515. 챔피언소트 （Small）/챔피언소트 （Small）.py
+++ 백준/Platinum/12515. 챔피언소트 （Small）/챔피언소트 （Small）.py
@@ -1,43 +1,17 @@
 # 백준 12515
 
 import io
-from collections import deque
 
 input = io.BufferedReader(io.FileIO(0), 1<<18).readline
 
 
-def BFS(start, graph, visited):
-    q = deque()
-    q.append(start)
-    visited[start] = True
-    count = 1
-
-    while q:
-        curV = q.popleft()
-
-        for nextV in graph[curV]:
-            if visited[nextV] == False:
-                q.append(nextV)
-                visited[nextV] = True
-                count += 1
-
-    if count == 1:
-        return 0
-    return count
-
-
 def solve(N, A):
-    graph = [[] for _ in range(N+1)]
+    count = 0
     for i in range(1, N+1):
-        graph[i].append(A[i-1])
-    
-    visited = [False for _ in range(N+1)]
-    result = 0
-    for i in range(N):
-        if visited[i] == False:
-            result += BFS(i, graph, visited)
-    
-    return result
+        if A[i-1] != i:
+            count += 1
+
+    return count
 
 
 def main():
@@ -45,7 +19,7 @@ def main():
     for i in range(1, T+1):
         N = int(input())
         A = list(map(int, input().split()))
-    
+
         print(f"Case #{i}: {solve(N, A)}.000000")
 
 
