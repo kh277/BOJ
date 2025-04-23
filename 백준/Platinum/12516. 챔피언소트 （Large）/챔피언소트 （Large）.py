47fe3d80624fc854d66098f759d9f2c4f3f561f4
kh277 <113894741+kh277@users.noreply.github.com>
2025-04-23 14:45:43 +0900

    [Platinum III] Title: 챔피언소트 (Large), Time: 100 ms, Memory: 110936 KB -BaekjoonHub

---
+++ 백준/Platinum/12516. 챔피언소트 （Large）/챔피언소트 （Large）.py
@@ -0,0 +1,30 @@
+# 백준 12516
+
+'''
+12515와 코드 동일
+'''
+
+import io
+
+input = io.BufferedReader(io.FileIO(0), 1<<18).readline
+
+
+def solve(N, A):
+    count = 0
+    for i in range(1, N+1):
+        if A[i-1] != i:
+            count += 1
+
+    return count
+
+
+def main():
+    T = int(input())
+    for i in range(1, T+1):
+        N = int(input())
+        A = list(map(int, input().split()))
+
+        print(f"Case #{i}: {solve(N, A)}.000000")
+
+
+main()
