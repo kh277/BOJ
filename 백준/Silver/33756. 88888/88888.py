571b9d15c500f7c205132aeb2f21d28e372fd141
kh277 <113894741+kh277@users.noreply.github.com>
2025-04-26 23:10:32 +0900

    [Silver III] Title: 88888, Time: 220 ms, Memory: 112308 KB, Score: 33 point -BaekjoonHub

---
+++ 백준/Silver/33756. 88888/88888.py
@@ -0,0 +1,36 @@
+# 백준 33756
+
+import io
+
+input = io.BufferedReader(io.FileIO(0), 1<<18).readline
+
+
+def solve(N):
+    eight = [8]
+    for i in range(17):
+        eight.append(int(str(eight[i]) + '8'))
+    
+    cur = len(eight)-1
+    count = 0
+    while N > 0:
+        if count > 8:
+            break
+
+        if N < eight[cur]:
+            cur -= 1
+            continue
+
+        N -= eight[cur]
+        count += 1
+
+    return 'No' if count > 8 else 'Yes'
+
+
+def main():
+    T = int(input())
+    for _ in range(T):
+        N = int(input())
+        print(solve(N))
+
+
+main()
