48d42dfda0b75d6bd93365537b1c84797411a1b1
kh277 <113894741+kh277@users.noreply.github.com>
2025-05-20 11:03:28 +0900

    [Silver II] Title: 수익, Time: 204 ms, Memory: 117524 KB -BaekjoonHub

--- 백준/Silver/4097. 수익/수익.py
+++ 백준/Silver/4097. 수익/수익.py
@@ -5,6 +5,7 @@ DP[i] = 구간 [0, i]에서의 최대 연속합
 '''
 
 import io
+from array import array
 
 input = io.BufferedReader(io.FileIO(0), 1<<18).readline
 INF = 10**10
@@ -12,7 +13,7 @@ INF = 10**10
 
 def solve(N, A):
     result = -INF
-    DP = [0 for _ in range(N)]
+    DP = array('i', [0]) * N
     DP[0] = A[0]
     for i in range(1, N):
         DP[i] = max(A[i], DP[i-1]+A[i])
@@ -26,7 +27,7 @@ def main():
         N = int(input())
         if N == 0:
             break
-        A = []
+        A = array('i')
         for _ in range(N):
             A.append(int(input()))
         print(solve(N, A))
