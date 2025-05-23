1e9205c597ce18dc8f11d74d8d0cb2c0d44e53f6
kh277 <113894741+kh277@users.noreply.github.com>
2025-05-23 10:44:08 +0900

    [Silver III] Title: 전쟁 - 땅따먹기, Time: 2500 ms, Memory: 247696 KB -BaekjoonHub

--- 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.py
+++ 백준/Silver/1270. 전쟁 － 땅따먹기/전쟁 － 땅따먹기.py
@@ -6,35 +6,35 @@
 
 import io
 
-input = io.BufferedReader(io.FileIO(0), 1<<18).readline
-
 
 def MajorityVote(nums):
     candidate = None
     count = 0
-    for i in nums:
+    for i in range(1, nums[0]+1):
         # 후보가 실질적으로 0번 등장한 경우 -> 후보 교체
         if count == 0:
-            candidate = i
+            candidate = nums[i]
             count = 1
         # 후보가 등장한 경우
-        elif i == candidate:
+        elif nums[i] == candidate:
             count += 1
         # 후보가 등장하지 않은 경우
         else:
             count -= 1
     
-    if candidate != None and nums.count(candidate) > len(nums)//2:
+    if candidate != None and nums.count(candidate) > (len(nums)-1)//2:
         return candidate
 
     return 'SYJKGW'
 
 
 def main():
+    input = io.BufferedReader(io.FileIO(0), 1<<18).readline
+
     N = int(input())
     for _ in range(N):
         T = list(map(int, input().split()))
-        print(MajorityVote(T[1:]))
+        print(MajorityVote(T))
 
 
 main()
