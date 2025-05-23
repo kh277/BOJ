1441ade970128f290958e1c3c1fb62dabdcba600
kh277 <113894741+kh277@users.noreply.github.com>
2025-05-23 16:33:31 +0900

    [Silver III] Title: 대한민국을 지키는 가장 긴 힘, Time: 112 ms, Memory: 111260 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/31263. 대한민국을 지키는 가장 긴 힘/대한민국을 지키는 가장 긴 힘.py
+++ 백준/Silver/31263. 대한민국을 지키는 가장 긴 힘/대한민국을 지키는 가장 긴 힘.py
@@ -13,13 +13,13 @@ def solve(N, S):
     DP[0] = 1
     if N > 1 and 10 <= int(S[:2]) <= 99:
         DP[1] = 1
-    if N > 2 and 100 <= int(S[:3]) <= 631:
+    if N > 2 and 100 <= int(S[:3]) <= 641:
         DP[2] = 1
 
     # DP값 처리
     for i in range(1, N):
         # 길이 3 체크
-        if i+2 < N and 100 <= int(S[i:i+3]) <= 631:
+        if i+2 < N and 100 <= int(S[i:i+3]) <= 641:
             DP[i+2] = min(DP[i+2], DP[i-1]+1)
         # 길이 2 체크
         if i+1 < N and 10 <= int(S[i:i+2]) <= 99:
