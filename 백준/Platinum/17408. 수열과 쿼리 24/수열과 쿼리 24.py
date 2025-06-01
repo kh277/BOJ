8305134011632e7bdd7e037e7fc393a2cc27827f
kh277 <113894741+kh277@users.noreply.github.com>
2025-06-01 21:37:49 +0900

    [Platinum IV] Title: 수열과 쿼리 24, Time: 408 ms, Memory: 124460 KB -BaekjoonHub

--- 백준/Platinum/17408. 수열과 쿼리 24/수열과 쿼리 24.py
+++ 백준/Platinum/17408. 수열과 쿼리 24/수열과 쿼리 24.py
@@ -1,12 +1,14 @@
 # 백준 17408
 
 '''
-세그먼트 트리의 각 노드는 [구간 내의 제일 큰 값, 구간 내의 두 번째로 큰 값]을 저장한다.
+세그먼트 트리 2개로 구간 내의 제일 큰 값, 구간 내의 두 번째로 큰 값을 저장한다.
 '''
 
 import io
+from array import array
 
 input = io.BufferedReader(io.FileIO(0), 1<<18).readline
+ARRAY_TYPE = 'I'
 
 
 # num이상의 2의 제곱수 반환
@@ -20,34 +22,39 @@ def square(num):
 
 # 크기가 N인 세그먼트 트리 빌드
 def build(N, A):
-    tree = [[0, 0] for _ in range(N*2)]
-    for i in range(len(A)):
-        tree[N+i] = [A[i], 0]
+    tree1 = array(ARRAY_TYPE, [0]) * (N*2)
+    tree2 = array(ARRAY_TYPE, [0]) * (N*2)
 
-    for i in range(N-1, 0, -1):
-        if tree[i<<1][0] > tree[i<<1 | 1][0]:
-            tree[i] = [tree[i<<1][0], max(tree[i<<1][1], tree[i<<1 | 1][0])]
+    for index in range(len(A)):
+        tree1[N+index] = A[index]
+
+    for index in range(N-1, 0, -1):
+        if tree1[index<<1] > tree1[index<<1 | 1]:
+            tree1[index] = tree1[index<<1]
+            tree2[index] = max(tree2[index<<1], tree1[index<<1 | 1])
         else:
-            tree[i] = [tree[i<<1 | 1][0], max(tree[i<<1][0], tree[i<<1 | 1][1])]
-
-    return tree
+            tree1[index] = tree1[index<<1 | 1]
+            tree2[index] = max(tree1[index<<1], tree2[index<<1 | 1])
+    return tree1, tree2
 
 
 # index번째 값을 value로 변경
-def update(N, tree, index, value):
+def update(N, tree1, tree2, index, value):
     index += N
-    tree[index] = [value, 0]
+    tree1[index] = value
 
     while index > 1:
         index >>= 1
-        if tree[index<<1][0] > tree[index<<1 | 1][0]:
-            tree[index] = [tree[index<<1][0], max(tree[index<<1][1], tree[index<<1 | 1][0])]
+        if tree1[index<<1] > tree1[index<<1 | 1]:
+            tree1[index] = tree1[index<<1]
+            tree2[index] = max(tree2[index<<1], tree1[index<<1 | 1])
         else:
-            tree[index] = [tree[index<<1 | 1][0], max(tree[index<<1][0], tree[index<<1 | 1][1])]
+            tree1[index] = tree1[index<<1 | 1]
+            tree2[index] = max(tree1[index<<1], tree2[index<<1 | 1])
 
 
 # 0-base, 구간 [left, right]에서 제일 큰 값과 두 번째로 큰 값을 더한 값 반환
-def query(N, tree, left, right):
+def query(N, tree1, tree2, left, right):
     result1 = 0
     result2 = 0
     left += N
@@ -55,18 +62,18 @@ def query(N, tree, left, right):
 
     while left <= right:
         if left & 1:
-            if tree[left][0] > result1:
-                result2 = max(result1, tree[left][1])
-                result1 = tree[left][0]
+            if tree1[left] > result1:
+                result2 = max(result1, tree2[left])
+                result1 = tree1[left]
             else:
-                result2 = max(result2, tree[left][0])
+                result2 = max(result2, tree1[left])
             left += 1
         if ~right & 1:
-            if tree[right][0] > result1:
-                result2 = max(result1, tree[right][1])
-                result1 = tree[right][0]
+            if tree1[right] > result1:
+                result2 = max(result1, tree2[right])
+                result1 = tree1[right]
             else:
-                result2 = max(result2, tree[right][0])
+                result2 = max(result2, tree1[right])
             right -= 1
         left >>= 1
         right >>= 1
@@ -77,15 +84,15 @@ def query(N, tree, left, right):
 def main():
     N = int(input())
     size = square(N)
-    tree = build(size, list(map(int, input().split())))
+    tree1, tree2 = build(size, list(map(int, input().split())))
 
     M = int(input())
     for _ in range(M):
         a, b, c = map(int, input().split())
         if a == 1:
-            update(size, tree, b-1, c)
+            update(size, tree1, tree2, b-1, c)
         else:
-            print(query(size, tree, b-1, c-1))
+            print(query(size, tree1, tree2, b-1, c-1))
 
 
 main()
