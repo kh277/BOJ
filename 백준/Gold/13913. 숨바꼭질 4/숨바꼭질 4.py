fb84f38d2b5dd68dfbe352e62b03d2eed02cf135
kh277 <113894741+kh277@users.noreply.github.com>
2025-06-15 00:58:46 +0900

    [Gold IV] Title: 숨바꼭질 4, Time: 140 ms, Memory: 120940 KB -BaekjoonHub

--- 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
+++ 백준/Gold/13913. 숨바꼭질 4/숨바꼭질 4.py
@@ -10,21 +10,30 @@ INF = 150000
 
 def BFS(start, end):
     if start <= end:
-        visited = array('i', [0]) * INF
+        visited = array('i', [-1]) * INF
         q = deque()
-        q.append([start, 0, [start]])
-        visited[start] = 1
+        q.append([start, 0])
+        visited[start] = start
 
         while q:
-            curNum, curCount, curVisit = q.popleft()
+            curNum, curCount = q.popleft()
 
             if curNum == end:
-                return [[curCount], curVisit]
+                cur = end
+                traceback = []
+                while True:
+                    if cur == start:
+                        traceback.append(start)
+                        break
+                    traceback.append(cur)
+                    cur = visited[cur]
+
+                return [[curCount], traceback[::-1]]
 
             for nextNum in [curNum+1, curNum-1, curNum*2]:
-                if 0 <= nextNum < INF and visited[nextNum] == 0:
-                    q.append([nextNum, curCount+1, curVisit + [nextNum]])
-                    visited[nextNum] = 1
+                if 0 <= nextNum < INF and visited[nextNum] == -1:
+                    q.append([nextNum, curCount+1])
+                    visited[nextNum] = curNum
 
     else:
         return [[start-end], [i for i in range(start, end-1, -1)]]
