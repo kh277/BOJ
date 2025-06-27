5148d90cf69f7e29511c57303dc8f28f6c4da50d
kh277 <113894741+kh277@users.noreply.github.com>
2025-06-27 17:08:08 +0900

    [Silver I] Title: 효율적인 해킹, Time: 13488 ms, Memory: 178996 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.py
@@ -1,88 +1,46 @@
 # 백준 1325
 
-'''
-A가 B를 신뢰할 경우 B를 해킹하면 A도 함께 해킹할 수 있게 된다.
-이것을 B->A로 가는 간선으로 생각한다.
-그러면 한 정점에서 가장 많은 칸을 이동할 수 있는 점은 무엇인가? 로 바뀌게 된다.
-
-각 정점마다 BFS로 최대 길이를 탐색하자.
-
-주의 반례
-3 2
-1 1
-2 2
--> 1, 2, 3
-
-5 4
-3 2
-4 3
-3 4
-4 5
--> 2, 5
-
-7 5
-2 1
-3 1
-4 1
-6 5
-7 6
--> 1
-'''
-
-import sys
+import io
+from array import array
 from collections import deque
 
-input = sys.stdin.readline
+input = io.BufferedReader(io.FileIO(0), 1<<18).readline
 
 
-# start 정점에서 제일 멀리 떨어진 정점까지의 거리 구하기
-def BFS(N: int, graph: list, start: list) -> list:
-    visited = [0 for _ in range(N+1)]
-    count = 1
-
-    q = deque([start])
-    visited[start] = 1
+def BFS(N, start, edge):
+    q = deque()
+    q.append(start)
+    visited = bytearray(N+1)
+    visited[start] ^= 1
 
     while q:
-        cur = q.popleft()
-        count += 1
+        curV = q.popleft()
 
-        for next in graph[cur]:
-            if visited[next] == 0:
-                q.append(next)
-                visited[next] = 1
-        
-    return count
-
-
-def solve(N: int, graph: list) -> int:
-    hack_count = [-1 for i in range(N+1)]
-
-    # 각 정점에 대해 해킹되는 컴퓨터의 수 도출
-    for i in range(1, N+1):
-        hack_count[i] = BFS(N, graph, i)
-
-    # 최대 데이터 도출
-    max_data = max(hack_count)
-
-    # 최대값과 중복이 되는 데이터가 있는지 확인
-    result = []
-    for i in range(1, N+1):
-        if hack_count[i] == max_data:
-            result.append(i)
+        for nextV in edge[curV]:
+            if visited[nextV] == 0:
+                q.append(nextV)
+                visited[nextV] ^= 1
     
-    return result
+    return sum(visited)
 
 
 def main():
     N, M = map(int, input().split())
+    edge = [array('h') for _ in range(N+1)]
+    for _ in range(M):
+        a, b = map(int, input().split())
+        edge[b].append(a)
 
-    graph = [[] for i in range(N+1)]
-    for i in range(M):
-        A, B = map(int, input().split())
-        graph[B].append(A)
-
-    print(*solve(N, graph))
+    result = []
+    maxResult = 0
+    for i in range(1, N+1):
+        temp = BFS(N, i, edge)
+        if temp > maxResult:
+            result = [i]
+            maxResult = temp
+        elif temp == maxResult:
+            result.append(i)
+    print(*sorted(result))
 
 
 main()
