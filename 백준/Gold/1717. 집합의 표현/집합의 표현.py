67c3b9124d136dad89e0bc259ddec445f7b8c563
kh277 <113894741+kh277@users.noreply.github.com>
2025-07-13 23:11:45 +0900

    [Gold V] Title: 집합의 표현, Time: 248 ms, Memory: 120640 KB -BaekjoonHub

--- 백준/Gold/1717. 집합의 표현/집합의 표현.py
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -1,30 +1,41 @@
 # 백준 1717
 
 import io
-from array import array
-import sys
 
 input = io.BufferedReader(io.FileIO(0), 1<<18).readline
-sys.setrecursionlimit(10**5)
 
 
 def find(parent, x):
-    if parent[x] != x:
-        parent[x] = find(parent, parent[x])
+    root = x
+    while parent[root] >= 0:
+        root = parent[root]
+    
+    while x != root:
+        nextN = parent[x]
+        parent[x] = root
+        x = nextN
 
-    return parent[x]
+    return root
 
 
-def union(parent, a, b):
-    a = find(parent, a)
-    b = find(parent, b)
+def union(parent, x, y):
+    x = find(parent, x)
+    y = find(parent, y)
 
-    parent[max(a, b)] = min(a, b)
+    if x == y:
+        return
+
+    if parent[x] > parent[y]:
+        parent[x] = y
+    else:
+        if parent[x] == parent[y]:
+            parent[x] -= 1
+        parent[y] = x
 
 
 def main():
     N, M = map(int, input().split())
-    parent = array('I', range(N+1))
+    parent = [-1] * (N+1)
     for _ in range(M):
         q, a, b = map(int, input().split())
         if q == 0:
@@ -37,4 +48,3 @@ def main():
 
 
 main()
-    
