c8a5de4d8b45cacd47b65059136ec13410669076
kh277 <113894741+kh277@users.noreply.github.com>
2025-07-22 13:28:07 +0900

    [Gold I] Title: 구간 곱 구하기, Time: 244 ms, Memory: 119476 KB -BaekjoonHub

--- 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.py
+++ 백준/Gold/11505. 구간 곱 구하기/구간 곱 구하기.py
@@ -1,64 +1,67 @@
 # 백준 11505
 
+import io
+from array import array
 
-import sys
+input = io.BufferedReader(io.FileIO(0), 1<<18).readline
+ARRAY_TYPE = 'I'
+MOD = 1000000007
 
-input = sys.stdin.readline
 
+# 크기가 N인 세그먼트 트리 빌드
+def build(N):
+    tree = array(ARRAY_TYPE, [0]) * (N*2)
+    for i in range(N):
+        tree[N+i] = int(input())
 
-# 세그먼트 트리 구성
-def init():
     for i in range(N-1, 0, -1):
-        tree[i] = (tree[i<<1] * tree[i<<1 | 1]) % 1000000007
+        tree[i] = (tree[i<<1] * tree[i<<1 | 1]) % MOD
+
+    return tree
 
 
-# index번째 인덱스를 value로 변경
-def update(index, value):
+# index번째 값을 value로 변경
+def update(N, tree, index, value):
     index += N
     tree[index] = value
     
     while index > 1:
         index >>= 1
-        tree[index] = (tree[index<<1] * tree[index<<1 | 1]) % 1000000007
+        tree[index] = (tree[index<<1] * tree[index<<1 | 1]) % MOD
 
 
-# left ~ right까지의 합 계산
-def query(left, right):
+# 0-base, 구간 [left, right]의 전체 곱을 구하는 쿼리
+def query(N, tree, left, right):
     result = 1
     left += N
     right += N
-    
+
     while left <= right:
-        # left가 홀수라면
         if left & 1:
-            result = (result * tree[left]) % 1000000007
+            result = (result * tree[left]) % MOD
             left += 1
-        # right가 홀수라면
         if ~right & 1:
-            result = (result * tree[right]) % 1000000007
+            result = (result * tree[right]) % MOD
             right -= 1
-            
         left >>= 1
         right >>= 1
-    
+
     return result
 
 
-# main 함수 ----------
-N, M, K = map(int, input().split())
+def main():
+    N, M, K = map(int, input().split())
 
-# 세그먼트 트리 설정
-tree = [0 for _ in range(2*N)]
+    # 세그먼트 트리 설정
+    tree = build(N)
 
-for i in range(N):
-    tree[i+N] = int(input())
+    # 쿼리 입력 및 처리
+    for _ in range(M+K):
+        x, y, z = map(int, input().split())
+        if x == 1:
+            update(N, tree, y-1, z)
+        else:
+            print(query(N, tree, y-1, z-1))
 
-init()
 
-# 쿼리 입력 및 처리
-for _ in range(M+K):
-    x, y, z = map(int, input().split())
-    if x == 1:
-        update(y-1, z)
-    else:
-        print(query(y-1, z-1))
+main()
