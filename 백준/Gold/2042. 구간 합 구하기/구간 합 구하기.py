f6ca7bd3a15b3510cf84abb82f4275e2b032d58c
kh277 <113894741+kh277@users.noreply.github.com>
2025-07-23 16:43:52 +0900

    [Gold I] Title: 구간 합 구하기, Time: 316 ms, Memory: 127240 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.py
@@ -7,60 +7,61 @@ input = io.BufferedReader(io.FileIO(0), 1<<18).readline
 ARRAY_TYPE = 'q'
 
 
-# 크기가 N인 세그먼트 트리 빌드
-def build(N):
-    tree = array(ARRAY_TYPE, [0]) * (N*2)
-    for i in range(N):
-        tree[N+i] = int(input())
+class SegmentTree:
+    # 크기가 N인 세그먼트 트리 빌드
+    def __init__(self, N):
+        self.size = N
+        self.tree = array(ARRAY_TYPE, [0]) * (self.size*2)
 
-    for i in range(N-1, 0, -1):
-        tree[i] = tree[i<<1] + tree[i<<1 | 1]
+        for i in range(N):
+            self.tree[N+i] = int(input())
 
-    return tree
-
-
-# index번째 값을 value로 변경
-def update(N, tree, index, value):
-    index += N
-    tree[index] = value
-    
-    while index > 1:
-        index >>= 1
-        tree[index] = tree[index<<1] + tree[index<<1 | 1]
+        for i in range(N-1, 0, -1):
+            self.tree[i] = self.tree[i<<1] + self.tree[i<<1 | 1]
 
 
-# 0-base, 구간 [left, right]의 전체 합을 구하는 쿼리
-def query(N, tree, left, right):
-    result = 0
-    left += N
-    right += N
+    # index번째 값을 value로 변경
+    def update(self, index, value):
+        index += self.size
+        self.tree[index] = value
+        
+        while index > 1:
+            index >>= 1
+            self.tree[index] = self.tree[index<<1] + self.tree[index<<1 | 1]
 
-    while left <= right:
-        if left & 1:
-            result += tree[left]
-            left += 1
-        if ~right & 1:
-            result += tree[right]
-            right -= 1
-        left >>= 1
-        right >>= 1
 
-    return result
+    # 0-base, 구간 [left, right]의 전체 합을 구하는 쿼리
+    def query(self, left, right):
+        result = 0
+        left += self.size
+        right += self.size
+
+        while left <= right:
+            if left & 1:
+                result += self.tree[left]
+                left += 1
+            if ~right & 1:
+                result += self.tree[right]
+                right -= 1
+            left >>= 1
+            right >>= 1
+
+        return result
 
 
 def main():
     N, M, K = map(int, input().split())
 
     # 세그먼트 트리 설정
-    tree = build(N)
+    t = SegmentTree(N)
 
     # 쿼리 입력 및 처리
     for _ in range(M+K):
         x, y, z = map(int, input().split())
         if x == 1:
-            update(N, tree, y-1, z)
+            t.update(y-1, z)
         else:
-            print(query(N, tree, y-1, z-1))
+            print(t.query(y-1, z-1))
 
 
 main()
