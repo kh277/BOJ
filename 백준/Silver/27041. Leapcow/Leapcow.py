4c4adbefa0cba419c328289f16aab449a9baa26d
kh277 <113894741+kh277@users.noreply.github.com>
2025-08-04 10:23:55 +0900

    [Silver V] Title: Leapcow, Time: 112 ms, Memory: 109764 KB -BaekjoonHub

--- 백준/Silver/27041. Leapcow/Leapcow.py
+++ 백준/Silver/27041. Leapcow/Leapcow.py
@@ -1,29 +1,26 @@
 # 백준 27041
 
-'''
-DP[i] = 소가 i칸에 도착하기 위해 도약해야 하는 횟수
-'''
-
 import io
 
 input = io.BufferedReader(io.FileIO(0), 1<<18).readline
 INF = 10**5
 
 
-def solve(goalX, maxLeap, B, cows):
-    DP = [INF for _ in range(goalX+1)]
-    DP[0] = 0
+def solve(goalX, maxLeap, cows):
+    curX = 0
+    count = 0
+    while True:
+        if curX == goalX:
+            break
 
-    for curX in range(0, goalX):
-        if curX in cows:
-            continue
-        for leap in range(1, maxLeap+1):
-            if curX+leap in cows:
-                continue
-            if curX+leap <= goalX:
-                DP[curX+leap] = min(DP[curX+leap], DP[curX]+1)
-    
-    return DP[goalX]
+        for leap in range(maxLeap, 0, -1):
+            nextX = curX+leap
+            if nextX <= goalX and nextX not in cows:
+                curX = nextX
+                count += 1
+                break
+
+    return count
 
 
 def main():
@@ -31,7 +28,7 @@ def main():
     cows = set()
     for _ in range(B):
         cows.add(int(input()))
-    print(solve(E, L, B, cows))
+    print(solve(E, L, cows))
 
 
 main()
