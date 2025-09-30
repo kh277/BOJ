2efd179504aab3665f1dc100738e8b78d6e309bb
kh277 <113894741+kh277@users.noreply.github.com>
2025-09-30 10:33:55 +0900

    [Silver II] Title: 키트 분배하기, Time: 128 ms, Memory: 135496 KB, Score: 9 point -BaekjoonHub

---
+++ 백준/Silver/24938. 키트 분배하기/키트 분배하기.py
@@ -0,0 +1,26 @@
+# 백준 24938
+
+import io
+
+input = io.BufferedReader(io.FileIO(0), 1<<18).readline
+
+
+def solve(N, A):
+    need = sum(A)//N
+    result = 0
+    left = 0
+    for i in range(N):
+        gap = need - A[i]
+        left += gap
+        result += left
+
+    return result
+
+
+def main():
+    N = int(input())
+    A = list(map(int, input().split()))
+    print(solve(N, A))
+
+
+main()
