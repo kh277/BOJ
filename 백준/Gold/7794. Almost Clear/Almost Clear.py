3ae328868f74a5084f71a1759532097a0d5b371b
kh277 <113894741+kh277@users.noreply.github.com>
2025-10-22 13:32:18 +0900

    [Gold I] Title: Almost Clear, Time: 1796 ms, Memory: 113132 KB -BaekjoonHub

--- 백준/Gold/7794. Almost Clear/Almost Clear.py
+++ 백준/Gold/7794. Almost Clear/Almost Clear.py
@@ -5,50 +5,24 @@ import io
 input = io.BufferedReader(io.FileIO(0), 1<<18).readline
 
 
-# 선분 교차 판정 서브 함수1
-def checkRange(A, B, C):
-    for i in range(2):
-        if not min(A[i], B[i]) <= C[i] <= max(A[i], B[i]):
-            return False
-    return True
-
-
-# 선분 교차 판정 서브 함수2
-def coef(A, B):
-    if A[0] == B[0]:
-        return [1, 0, A[0]]
-    elif A[1] == B[1]:
-        return [0, 1, A[1]]
-    else:
-        return [B[1]-A[1], A[0]-B[0], (B[1]-A[1])*A[0] - (B[0]-A[0])*A[1]]
+# 선분 교차 판정 서브 함수
+def CCW(A, B, C):
+    return (B[0]-A[0]) * (C[1]-A[1]) - (B[1]-A[1]) * (C[0]-A[0])
 
 
 # 선분 교차 판정
-def lineIntetsection(A, B, C, D):
-    a, b, e = coef(A, B)
-    c, d, f = coef(C, D)
+def LineIntersection(A, B, C, D):
+    AB = CCW(A, B, C) * CCW(A, B, D)
+    CD = CCW(C, D, A) * CCW(C, D, B)
 
-    det = a*d - b*c
-    if det == 0:
-        if e == f:
-            if checkRange(A, B, C) or checkRange(A, B, D) or checkRange(C, D, A) or checkRange(C, D, B):
-                if B[0] == C[0] and B[1] == C[1]:
-                    return 1
-                elif A[0] == D[0] and A[1] == D[1]:
-                    return 1
-                else:
-                    return 1
-            else:
-                return 0
-        else:
-            return 0
+    if AB == 0 and CD == 0:
+        if B < A:
+            A, B = B, A
+        if D < C:
+            C, D = D, C
+        return not (B < C or D < A)
 
-    x = (e*d-b*f) / det
-    y = (a*f-e*c) / det
-    if checkRange(A, B, [x, y]) and checkRange(C, D, [x, y]):
-        return 1
-    else:
-        return 0
+    return AB <= 0 and CD <= 0
 
 
 def solve(A, B, pointA, pointB, cameraP):
@@ -57,7 +31,7 @@ def solve(A, B, pointA, pointB, cameraP):
     for i in range(A):
         count = 0
         for j in range(B):
-            if lineIntetsection(cameraP, pointA[i], pointB[j], pointB[(j+1)%B]) == 1:
+            if LineIntersection(cameraP, pointA[i], pointB[j], pointB[(j+1)%B]) == 1:
                 count += 1
                 break
         if count > 0:
