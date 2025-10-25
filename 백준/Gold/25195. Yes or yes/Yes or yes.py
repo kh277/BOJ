45d037149a74a6a86f8794404e223a0f3ff93305
kh277 <113894741+kh277@users.noreply.github.com>
2025-10-26 00:34:31 +0900

    [Gold IV] Title: Yes or yes, Time: 140 ms, Memory: 143664 KB -BaekjoonHub

---
+++ 백준/Gold/25195. Yes or yes/Yes or yes.py
@@ -0,0 +1,39 @@
+# 백준 25195
+
+import io
+
+input = io.BufferedReader(io.FileIO(0), 1<<18).readline
+
+
+def DFS(graph, fan):
+    start = 1
+    stack = []
+    stack.append(start)
+
+    while stack:
+        curV = stack.pop()
+
+        if curV in fan:
+            continue
+        
+        if len(graph[curV]) == 0:
+            return 'yes'
+
+        for nextV in graph[curV]:
+            stack.append(nextV)
+
+    return 'Yes'
+
+
+def main():
+    N, M = map(int, input().split())
+    graph = [[] for _ in range(N+1)]
+    for _ in range(M):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+    S = int(input())
+    fan = set(map(int, input().split()))
+    print(DFS(graph, fan))
+
+
+main()
