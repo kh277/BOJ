b9c42f70fcbd061c99814c7fbba525963dc8ee7c
kh277 <113894741+kh277@users.noreply.github.com>
2025-10-27 15:15:27 +0900

    [Gold IV] Title: 파일 합치기 3, Time: 2180 ms, Memory: 274136 KB -BaekjoonHub

---
+++ 백준/Gold/13975. 파일 합치기 3/파일 합치기 3.py
@@ -0,0 +1,33 @@
+# 백준 13975
+
+'''
+그리디하게 파일 중 제일 작은 파일 2개를 합치는 과정을 반복하면 최적해가 나온다.
+'''
+
+import io
+import heapq
+
+input = io.BufferedReader(io.FileIO(0), 1<<18).readline
+
+def solve(N, L):
+    heapq.heapify(L)
+
+    cost = 0
+    while len(L) > 1:
+        A = heapq.heappop(L)
+        B = heapq.heappop(L)
+        cost += A+B
+        heapq.heappush(L, A+B)
+
+    return cost
+
+
+def main():
+    T = int(input())
+    for _ in range(T):
+        N = int(input())
+        L = list(map(int, input().split()))
+        print(solve(N, L))
+
+
+main()
