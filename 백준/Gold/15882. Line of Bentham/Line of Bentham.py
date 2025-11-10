4394be3db305e7010cdb665980f1ee39f6011261
kh277 <113894741+kh277@users.noreply.github.com>
2025-11-10 16:59:47 +0900

    [Gold I] Title: Line of Bentham, Time: 740 ms, Memory: 249072 KB -BaekjoonHub

--- 백준/Gold/15882. Line of Bentham/Line of Bentham.py
+++ 백준/Gold/15882. Line of Bentham/Line of Bentham.py
@@ -11,29 +11,28 @@ DP[i]는 DP[i-1]에만 의존하므로 일차원 DP로 압축할 수 있다.
 '''
 
 import io
-from array import array
 
 input = io.BufferedReader(io.FileIO(0), 1<<18).readline
 INF = 10**9
 
 
 def solve(N, data):
-    prevDP = array('i', [0, 0, 0, data[2][2], 0, data[2][1], data[1][2], data[1][2]+data[2][1]+data[2][2]])
+    prevDP = [0, 0, 0, data[2][2], 0, data[2][1], data[1][2], data[1][2]+data[2][1]+data[2][2]]
 
     for i in range(4, N+1):
-        DP = array('i', [-INF]) * 8
+        DP = [-INF] * 8
         for status in range(8):
             # 이전 상태의 요원 여부 추출
             prev3 = (status >> 2) & 1
             prev2 = (status >> 1) & 1
             prev1 = status & 1
 
-            # i번째 사람을 그대로 두는 경우
+            # i번째 사람을 요원으로 대체하는 경우
             nextStatus = (status & 3) << 1
             if DP[nextStatus] < prevDP[status]:
                 DP[nextStatus] = prevDP[status]
 
-            # i번째 사람을 요원으로 대체하는 경우
+            # i번째 사람을 그대로 유지하는 경우
             nextStatus += 1
             add = prev3*data[i-1][0] + prev2*data[i-1][1] + prev1*data[i-1][2]
             DP[nextStatus] = max(DP[nextStatus], prevDP[status] + add)
