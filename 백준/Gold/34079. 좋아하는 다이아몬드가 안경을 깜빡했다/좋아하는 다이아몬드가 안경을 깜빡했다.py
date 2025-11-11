66024070c3d40f939a5d483f7ea69d2f1e2b15b5
kh277 <113894741+kh277@users.noreply.github.com>
2025-11-11 14:51:01 +0900

    [Gold II] Title: 좋아하는 다이아몬드가 안경을 깜빡했다, Time: 416 ms, Memory: 149816 KB -BaekjoonHub

--- 백준/Gold/34079. 좋아하는 다이아몬드가 안경을 깜빡했다/좋아하는 다이아몬드가 안경을 깜빡했다.py
+++ 백준/Gold/34079. 좋아하는 다이아몬드가 안경을 깜빡했다/좋아하는 다이아몬드가 안경을 깜빡했다.py
@@ -1,15 +1,16 @@
 # 백준 34079
 
 import io
+from array import array
 from collections import deque
 
 input = io.BufferedReader(io.FileIO(0), 1<<18).readline
 INF = 10**6
 
 
-def BFS(N, graph, start, visited):
+def BFS(N, graph, start):
     q = deque()
-    traceback = [[] for _ in range(N+1)]
+    visited = array('i', [INF]) * (N+1)
     q.append((start, 0))
     visited[start] = 0
 
@@ -20,33 +21,29 @@ def BFS(N, graph, start, visited):
         for nextV in graph[curV]:
             if visited[nextV] > nextM:
                 visited[nextV] = nextM
-                traceback[nextV] = [curV]
                 q.append((nextV, nextM))
-            elif visited[nextV] == nextM:
-                traceback[nextV].append(curV)
 
-    return traceback
+    return visited
 
 
 def solve(N, graph):
-    # 학교 -> 집으로 가는 최단 거리 탐색
-    visited = [INF for _ in range(N+1)]
-    traceback = BFS(N, graph, 1, visited)
+    # 시작 정점을 1번, N번 정점으로 잡고 BFS 탐색
+    dist1 = BFS(N, graph, 1)
+    distN = BFS(N, graph, N)
 
-    # 역추적 경로를 그래프로 잡고 다시 BFS 탐색
-    visited2 = [INF for _ in range(N+1)]
-    BFS(N, traceback, N, visited2)
-
-    # 최단 경로에 포함된 정점 중 이동 거리가 동일한 정점끼리 모으기
-    count = [[] for _ in range(N+1)]
+    # 최단 거리에 포함되는 정점까지의 이동 거리 및 정점 번호 저장
+    minD = dist1[N]
+    count = array('i', [0]) * (minD+1)
+    index = array('i', [0]) * (minD+1)
     for i in range(1, N+1):
-        if visited2[i] != INF:
-            count[visited2[i]].append(i)
+        if dist1[i] <= minD and dist1[i]+distN[i] == minD:
+            count[dist1[i]] += 1
+            index[dist1[i]] = i
 
-    # 이동 거리가 동일한 정점이 1개뿐인 경우 탐색
-    for i in range(1, N+1):
-        if len(count[i]) == 1:
-            return count[i]
+    # 이동 거리가 유일한 정점 반환
+    for i in range(minD-1, -1, -1):
+        if count[i] == 1:
+            return index[i]
     return 1
 
 
@@ -58,7 +55,7 @@ def main():
         graph[s].append(e)
         graph[e].append(s)
 
-    print(*solve(N, graph))
+    print(solve(N, graph))
 
 
 main()
