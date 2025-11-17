aba2579d92be7f54472ac341c7bba1374f37c514
kh277 <113894741+kh277@users.noreply.github.com>
2025-11-17 13:52:15 +0900

    [Platinum V] Title: #15164번_제보, Time: 356 ms, Memory: 219428 KB -BaekjoonHub

---
+++ 백준/Platinum/16163. #15164번＿제보/#15164번＿제보.py
@@ -0,0 +1,37 @@
+# 백준 13275
+
+import io
+from array import array
+
+input = io.BufferedReader(io.FileIO(0), 1<<18).readline
+
+
+def Manacher(string):
+    # 전처리 - 짝수 길이 팰린드롬을 처리하기 위한 구분자 추가
+    text = '$#' + '#'.join(string) + '#@'
+
+    # 매내처 알고리즘으로 보조 배열 P 계산하기
+    P = array('I', [1]) * len(text)
+    radius = 0
+    center = 0
+
+    for i in range(1, len(text)-1):
+        if i < radius:
+            P[i] = min(P[2*center-i], radius-i)
+
+        while text[i-P[i]] == text[i+P[i]]:
+            P[i] += 1
+
+        if i+P[i] > radius:
+            radius = i + P[i]
+            center = i
+
+    return sum([P[i]//2 for i in range(1, len(P)-1)])
+
+
+def main():
+    string = input().decode().strip()
+    print(Manacher(string))
+
+
+main()
