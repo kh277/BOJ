d7916e082a70f672942a7bc6acd336589259ad5d
kh277 <113894741+kh277@users.noreply.github.com>
2025-11-28 15:54:52 +0900

    [Platinum IV] Title: 구간 합 구하기 2, Time: 572 ms, Memory: 209500 KB -BaekjoonHub

--- 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.py
+++ 백준/Platinum/10999. 구간 합 구하기 2/구간 합 구하기 2.py
@@ -3,45 +3,95 @@
 import io
 
 input = io.BufferedReader(io.FileIO(0), 1<<18).readline
+INF = 10**10
 
 
-# addQuery에서 구간 [left, right]에 변경된 값의 총합 반환
-def query(left, right, addQuery):
-    result = 0
-    for start, end, value in addQuery:
-        if right >= start and end >= left:
-            result += (min(right, end)-max(left, start)+1)*value
+# [start, end] 범위의 세그먼트 트리 생성
+def build(tree, A, curV, start, end):
+    # 리프 노드인 경우
+    if start == end:
+        tree[curV] = A[start]
+        return tree[curV]
 
-    return result
+    mid = (start+end)//2
+    tree[curV] = build(tree, A, curV<<1, start, mid) + build(tree, A, curV<<1 | 1, mid+1, end)
+    return tree[curV]
+
+
+# 세그먼트 트리의 [start, end] 범위에서 [qLeft, qRight] 범위의 합 반환
+def query(tree, lazy, curV, start, end, qLeft, qRight):
+    # 현재 노드의 lazy 먼저 처리
+    prop(tree, lazy, curV, start, end)
+
+    # 겹치는 구간이 없는 경우
+    if qLeft > end or qRight < start:
+        return 0
+
+    # 전체 범위인 경우
+    if qLeft <= start and end <= qRight:
+        return tree[curV]
+
+    mid = (start+end)//2
+    return query(tree, lazy, curV<<1, start, mid, qLeft, qRight) + query(tree, lazy, curV<<1 | 1, mid+1, end, qLeft, qRight)
+
+
+def prop(tree, lazy, curV, start, end):
+    # 업데이트가 필요한 경우
+    if lazy[curV] != 0:
+        tree[curV] += (end-start+1) * lazy[curV]
+
+        # 리프 노드가 아닌 경우
+        if start != end:
+            lazy[curV<<1] += lazy[curV]
+            lazy[curV<<1 | 1] += lazy[curV]
+
+        lazy[curV] = 0
+
+
+def lazyUpdate(tree, lazy, curV, start, end, qLeft, qRight, value):
+    # lazy값이 남아있으면 전파
+    prop(tree, lazy, curV, start, end)
+
+    if qLeft > end or qRight < start:
+        return
+    
+    if qLeft <= start and end <= qRight:
+        tree[curV] += (end-start+1) * value
+
+        # 리프 노드가 아니라면
+        if start != end:
+            lazy[curV<<1] += value
+            lazy[curV<<1 | 1] += value
+        return
+
+    mid = (start+end)//2
+    lazyUpdate(tree, lazy, curV<<1, start, mid, qLeft, qRight, value)
+    lazyUpdate(tree, lazy, curV<<1 | 1, mid+1, end, qLeft, qRight, value)
+    tree[curV] = tree[curV<<1] + tree[curV<<1 | 1]
 
 
 def main():
     N, M, K = map(int, input().split())
-
     A = []
     for _ in range(N):
         A.append(int(input()))
+    size = 4*N
+    tree = [0] * size
+    lazy = [0] * size
 
-    # A 누적합 계산
-    accSum = [0 for _ in range(N)]
-    accSum[0] = A[0]
-    for i in range(1, N):
-        accSum[i] = accSum[i-1] + A[i]
+    # 세그먼트 트리 빌드
+    build(tree, A, 1, 0, N-1)
 
     # 쿼리 처리
-    addQuery = []
     for _ in range(M+K):
         q = list(map(int, input().split()))
-        # 1: 더하기 쿼리 저장
+        # 1 : 구간 업데이트 쿼리
         if q[0] == 1:
-            addQuery.append([q[1]-1, q[2]-1, q[3]])
-        
-        # 2: 수열 누적 합 + 더하기 쿼리에서 범위에 포함되는 값 합산
+            lazyUpdate(tree, lazy, 1, 0, N-1, q[1]-1, q[2]-1, q[3])
+
+        # 2: 구간 합 쿼리
         else:
-            gap = accSum[q[2]-1]
-            if q[1]-1 > 0:
-                gap -= accSum[q[1]-2]
-            print(gap + query(q[1]-1, q[2]-1, addQuery))
+            print(query(tree, lazy, 1, 0, N-1, q[1]-1, q[2]-1))
 
 
 main()
