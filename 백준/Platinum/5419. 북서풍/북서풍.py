ee3547ec91d18390787a5e8e409ca4f506e3d322
kh277 <113894741+kh277@users.noreply.github.com>
2025-12-03 16:50:23 +0900

    [Platinum III] Title: 북서풍, Time: 1644 ms, Memory: 214604 KB -BaekjoonHub

--- 백준/Platinum/5419. 북서풍/북서풍.py
+++ 백준/Platinum/5419. 북서풍/북서풍.py
@@ -1,21 +1,16 @@
 # 백준 5419
 
 import io
+from array import array
 
 input = io.BufferedReader(io.FileIO(0), 1<<18).readline
-
-
-# 세그먼트 트리 구성
-def init(N, tree):
-    for i in range(N-1, 0, -1):
-        tree[i] = tree[i<<1] + tree[i<<1 | 1]
 
 
 # index번째 인덱스의 값을 value만큼 추가
 def update(N, tree, index, value):
     index += N
     tree[index] += value
-    
+
     while index > 1:
         index >>= 1
         tree[index] = tree[index<<1] + tree[index<<1 | 1]
@@ -37,7 +32,7 @@ def query(N, tree, left, right):
             
         left >>= 1
         right >>= 1
-    
+
     return result
 
 
@@ -53,30 +48,25 @@ def compress(N, point, value):
             before = point[i][value]
             compress += 1
             point[i][value] = compress
-    
+
     return point
 
 
 def solve(N, point):
-    # x, y좌표 압축
+    # 좌표 압축 및 정렬
     point = compress(N, point, 0)
     point = compress(N, point, 1)
-    
-    # y좌표가 커지도록, x좌표가 작아지도록 정렬
     point.sort(key= lambda x: (x[1], -x[0]))
 
-    # 세그먼트 트리 설정
-    tree = [0 for _ in range(N*2)]
+    tree = array('I', [0]) * (N*2)
 
     # y좌표가 작은 점들부터 세그먼트 트리에 추가 및 쿼리 처리
-    result = 0
+    total = 0
     for i in range(N):
         update(N, tree, point[i][0], 1)
+        total += query(N, tree, point[i][0], N-1) - 1
 
-        # 자기 자신을 제외한 우하단 섬의 개수 누적
-        result += query(N, tree, point[i][0], N-1) - 1
-
-    return result
+    return total
 
 
 def main():
