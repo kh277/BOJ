98df86d3fe7acd8cbc1e673ac21dc13705a7f164
kh277 <113894741+kh277@users.noreply.github.com>
2025-12-17 13:46:14 +0900

    [Gold II] Title: 풍선 수집, Time: 116 ms, Memory: 111152 KB -BaekjoonHub

--- 백준/Gold/3866. 풍선 수집/풍선 수집.py
+++ 백준/Gold/3866. 풍선 수집/풍선 수집.py
@@ -1,7 +1,8 @@
 # 백준 3866
 
 '''
-DP[i][x][c] = 현재 위치가 x이고 들고 있는 풍선이 c개일 때, i번째 풍선까지 회수하기 위한 최소 이동 거리.
+DP[i][c] = 현재 들고 있는 풍선이 c개일 때, i번째 풍선까지 회수하기 위한 최소 이동 거리.
+DP[i]를 계산하기 위해서는 DP[i-1]만 필요하기 때문에 1차원으로 줄일 수 있다.
 '''
 
 import io
@@ -12,45 +13,42 @@ MAX_X = 100
 
 
 def solve(N, ball):
-    DP = [[[INF] * 4 for _ in range(MAX_X+1)] for _ in range(N+1)]
-    DP[0][0][0] = 0
-
+    prevDP = [INF] * 4
+    prevDP[0] = 0
     prevT = 0
+    prevX = 0
+
     for i in range(1, N+1):
+        DP = [INF] * 4
         ballX, ballT = ball[i-1]
         canCatch = False
 
         # 갖고 있는 풍선을 그대로 들고 현재 풍선을 잡는 경우
-        for prevX in range(MAX_X+1):
-            for count in range(3):
-                if DP[i-1][prevX][count] == INF:
-                    continue
-                robotT = prevT + abs(ballX - prevX)*(count+1)
-                if robotT <= ballT:
-                    canCatch = True
-                    DP[i][ballX][count+1] = min(DP[i][ballX][count+1], DP[i-1][prevX][count] + abs(ballX-prevX))
-        
+        for count in range(3):
+            robotT = prevT + abs(ballX - prevX)*(count+1)
+            if prevDP[count] != INF and robotT <= ballT:
+                canCatch = True
+                DP[count+1] = min(DP[count+1], prevDP[count] + abs(ballX-prevX))
+
         # 갖고 있는 풍선을 집에 보관한 뒤 현재 풍선을 잡는 경우
-        for prevX in range(MAX_X+1):
-            for count in range(1, 4):
-                if DP[i-1][prevX][count] == INF:
-                    continue
-                robotT = prevT + prevX*(count+1) + ballX
-                if robotT <= ballT:
-                    canCatch = True
-                    DP[i][ballX][1] = min(DP[i][ballX][1], DP[i-1][prevX][count] + prevX+ballX)
+        for count in range(1, 4):
+            robotT = prevT + prevX*(count+1) + ballX
+            if prevDP[count] != INF and robotT <= ballT:
+                canCatch = True
+                DP[1] = min(DP[1], prevDP[count] + prevX+ballX)
 
         # 풍선을 잡지 못한 경우
         if canCatch == False:
             return f"NG {i}"
         prevT = ballT
+        prevX = ballX
+        prevDP = DP
 
     # 최소 이동거리 도출
     result = INF
-    for curX in range(MAX_X+1):
-        for c in range(4):
-            result = min(result, DP[N][curX][c] + curX)
-    
+    for c in range(4):
+        result = min(result, prevDP[c] + prevX)
+
     return f"OK {result}"
 
 
