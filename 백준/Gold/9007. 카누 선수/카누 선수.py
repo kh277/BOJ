2483d455c68ec35b97f54ad91320922a5768a5ae
kh277 <113894741+kh277@users.noreply.github.com>
2026-01-05 11:20:56 +0900

    [Gold II] Title: 카누 선수, Time: 3380 ms, Memory: 253844 KB -BaekjoonHub

--- 백준/Gold/9007. 카누 선수/카누 선수.py
+++ 백준/Gold/9007. 카누 선수/카누 선수.py
@@ -5,68 +5,53 @@ import io
 input = io.BufferedReader(io.FileIO(0), 1<<18).readline
 
 
-# arr에서 target 이하인 값 중 최대의 값 탐색
-def binarySearch(arr, target):
-    yes = 0
-    no = len(arr)-1
-
-    while abs(yes-no) > 1:
-        mid = (yes+no)//2
-        if arr[mid] < target:
-            yes = mid
-        else:
-            no = mid
-
-    return yes
-
-
-def solve(N, K, data):
+def solve(N, K, a, b, c, d):
     # 반을 2개씩 묶기
-    A = set()
-    B = set()
+    A = []
+    B = []
     for i in range(N):
         for j in range(N):
-            A.add(data[0][i]+data[1][j])
-            B.add(data[2][i]+data[3][j])
+            A.append(a[i]+b[j])
+            B.append(c[i]+d[j])
 
     # 정렬
-    A = sorted(list(A))
-    B = sorted(list(B))
+    A.sort()
+    B.sort()
 
     # K에 가까워지도록 하는 A+B 값 찾기
-    gap = 10**9
-    result = 0
-    for iA in range(len(A)):
-        # B에서 K에 가까워지는 최적의 값 찾기
-        iB = binarySearch(B, K - A[iA])
-        curGap = abs(K-A[iA]-B[iB])
+    iA = 0
+    iB = len(B)-1
+    bestSum = 10**9
+    bestGap = 10**9
+    while iA < len(A) and iB >= 0:
+        curSum = A[iA] + B[iB]
+        diff = K - curSum
+        curGap = abs(diff)
 
-        # iB 근처 값도 조사
-        for i in range(iB, min(len(B), iB+3)):
-            temp = abs(K-A[iA]-B[i])
-            if curGap > temp:
-                curGap = temp
-                iB = i
+        if curGap < bestGap or (curGap == bestGap and curSum < bestSum):
+            bestSum = curSum
+            bestGap = curGap
+            if bestGap == 0:
+                return bestSum
 
-        # 값 갱신
-        if gap > curGap:
-            gap = curGap
-            result = A[iA]+B[iB]
-        elif gap == curGap:
-            result = min(result, A[iA]+B[iB])
-
-    return result
+        if diff > 0:
+            iA += 1
+        else:
+            iB -= 1
+    
+    return bestSum
 
 
 def main():
     T = int(input())
     for _ in range(T):
         K, N = map(int, input().split())
-        data = []
-        for _ in range(4):
-            data.append(list(map(int, input().split())))
+        a = list(map(int, input().split()))
+        b = list(map(int, input().split()))
+        c = list(map(int, input().split()))
+        d = list(map(int, input().split()))
 
-        print(solve(N, K, data))
+        print(solve(N, K, a, b, c, d))
 
 
 main()
