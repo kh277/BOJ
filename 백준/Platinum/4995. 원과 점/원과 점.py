fce31e6b09d05f8a611246b3e06456d2d6da2876
kh277 <113894741+kh277@users.noreply.github.com>
2026-01-20 12:06:50 +0900

    [Platinum II] Title: 원과 점, Time: 1432 ms, Memory: 112132 KB -BaekjoonHub

---
+++ 백준/Platinum/4995. 원과 점/원과 점.py
@@ -0,0 +1,52 @@
+# 백준 4995
+
+import io
+from math import sqrt
+
+input = io.BufferedReader(io.FileIO(0), 1<<18).readline
+r = 1
+EPS = 1e-12
+
+
+def solve(N, points):
+    maxCount = 1
+    for i in range(N-1):
+        for j in range(i+1, N):
+            # i번 점, j번 점이 원주 위에 있도록 원의 중심 좌표 구하기
+            dx = points[j][0]-points[i][0]
+            dy = points[j][1]-points[i][1]
+            d2 = dx*dx + dy*dy
+            if d2 > 4*r*r + EPS:
+                continue
+            d = sqrt(d2)
+            mX = (points[i][0] + points[j][0]) / 2
+            mY = (points[i][1] + points[j][1]) / 2
+            h = sqrt(max(0, r*r - d2/4))
+
+            circle = [(mX - dy*h/d, mY + dx*h/d), (mX + dy*h/d, mY - dx*h/d)]
+
+            # (cX, cY)가 중심 좌표일 때, 해당 원 내부에 존재하는 점의 개수 체크
+            for cX, cY in circle:
+                count = 0
+                for k in range(N):
+                    if (points[k][0]-cX)**2 + (points[k][1]-cY)**2 <= r*r + EPS:
+                        count += 1
+
+                maxCount = max(maxCount, count)
+
+    return maxCount
+
+
+def main():
+    while True:
+        N = int(input())
+        if N == 0:
+            break
+        points = []
+        for _ in range(N):
+            points.append(list(map(float, input().split())))
+
+        print(solve(N, points))
+
+
+main()
