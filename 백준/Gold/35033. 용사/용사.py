dddeb6439343eb7634539017bc8bfbdc1fe50018
kh277 <113894741+kh277@users.noreply.github.com>
2026-02-12 14:45:44 +0900

    [Gold I] Title: 용사, Time: 464 ms, Memory: 195524 KB -BaekjoonHub

--- 백준/Gold/35033. 용사/용사.py
+++ 백준/Gold/35033. 용사/용사.py
@@ -1,27 +1,38 @@
 # 백준 35033
 
-import io, sys
+import io
 
 input = io.BufferedReader(io.FileIO(0), 1<<18).readline
-sys.setrecursionlimit(300000)
 
 
 # graph에서 자식 정점의 개수 구하기
-def getTreeSize(V, graph, visited, subSize, curV):
-    count = 1
-    for nextV in graph[curV]:
-        if visited[nextV] == -1:
-            visited[nextV] += 1
-            count += getTreeSize(V, graph, visited, subSize, nextV)
-    
-    subSize[curV] = count
-    return count
+def getTreeSize(V, graph, visited, subSize, root):
+    # 루트에서 리프 노드로 내려가기
+    parent = [0 for _ in range(V+1)]
+    trace = [1]
+    stack = []
+    stack.append(root)
+    visited[root] += 1
+    parent[root] = -1
+
+    while stack:
+        curV = stack.pop()
+        for nextV in graph[curV]:
+            if visited[nextV] == -1:
+                visited[nextV] += 1
+                parent[nextV] = curV
+                stack.append(nextV)
+                trace.append(nextV)
+
+    # 뒤집어 다시 올라가며 서브트리 정점 수 계산
+    for curV in trace[::-1]:
+        subSize[parent[curV]] += subSize[curV]
 
 
 def solve(V, graph, root):
     # 각 서브트리에 속한 자식 정점의 개수 구하기
     visited = [-1 for _ in range(V+1)]
-    subSize = [0] * (V+1)
+    subSize = [1] * (V+1)
     getTreeSize(V, graph, visited, subSize, 1)
 
     # 연결된 자식 정점의 개수가 많은 것부터 우선 처리하도록 정렬
