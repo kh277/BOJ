830e48b2d8b2ed2ab2eed3ca7a1b24fdf74b9bb9
kh277 <113894741+kh277@users.noreply.github.com>
2026-02-12 14:11:11 +0900

    [Gold I] Title: 용사, Time: 988 ms, Memory: 707832 KB -BaekjoonHub

---
+++ 백준/Gold/35033. 용사/용사.py
@@ -0,0 +1,65 @@
+# 백준 35033
+
+import io, sys
+
+input = io.BufferedReader(io.FileIO(0), 1<<18).readline
+sys.setrecursionlimit(300000)
+
+
+# graph에서 자식 정점의 개수 구하기
+def getTreeSize(V, graph, visited, subSize, curV):
+    count = 1
+    for nextV in graph[curV]:
+        if visited[nextV] == -1:
+            visited[nextV] += 1
+            count += getTreeSize(V, graph, visited, subSize, nextV)
+    
+    subSize[curV] = count
+    return count
+
+
+def solve(V, graph, root):
+    # 각 서브트리에 속한 자식 정점의 개수 구하기
+    visited = [-1 for _ in range(V+1)]
+    subSize = [0] * (V+1)
+    getTreeSize(V, graph, visited, subSize, 1)
+
+    # 연결된 자식 정점의 개수가 많은 것부터 우선 처리하도록 정렬
+    for i in range(1, V+1):
+        graph[i].sort(key= lambda x: -subSize[x])
+
+    stack1 = []
+    stack2 = []
+    seq = []
+    stack1.append(root)
+    visited[root] = 1
+
+    # 비재귀 후위 순회
+    while stack1:
+        curV = stack1.pop()
+        stack2.append(curV)
+
+        for nextV in graph[curV]:
+            if visited[nextV] == 0:
+                stack1.append(nextV)
+                visited[nextV] = 1
+
+    while stack2:
+        curV = stack2.pop()
+        seq.append(curV)
+
+    return seq
+
+
+def main():
+    V = int(input())
+    graph = [[] for _ in range(V+1)]
+    for _ in range(V-1):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        graph[b].append(a)
+
+    print(*solve(V, graph, 1))
+
+
+main()
