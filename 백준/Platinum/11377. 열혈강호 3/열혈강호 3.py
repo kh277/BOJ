a86f83ca857b260bdc8b32af2ac76af2353f3e7f
kh277 <113894741+kh277@users.noreply.github.com>
2026-03-01 16:14:33 +0900

    [Platinum III] Title: 열혈강호 3, Time: 344 ms, Memory: 195772 KB -BaekjoonHub

--- 백준/Platinum/11377. 열혈강호 3/열혈강호 3.py
+++ 백준/Platinum/11377. 열혈강호 3/열혈강호 3.py
@@ -2,74 +2,94 @@
 
 import io
 from collections import deque
+INF = 10000000
 
 input = io.BufferedReader(io.FileIO(0), 1<<18).readline
-INF = 10**8
 
 
-def EdmondKarp(graph, capacity, source, sink):
-    totalFlow = 0
-    while True:
-        parent = [-1] * (sink+1)
-        q = deque()
-        q.append(source)
+class Dinic:
+    def __init__(self, size, source, sink):
+        self.V = size
+        self.graph = [[] for _ in range(size)]
+        self.level = [-1] * size
+        self.pointer = [0] * size
+        self.source = source
+        self.sink = sink
 
-        isFound = False
+
+    def addEdge(self, start, end, cap, directed=True):
+        self.graph[start].append([end, cap, len(self.graph[end])])
+        self.graph[end].append([start, 0 if directed else cap, len(self.graph[start])-1])
+
+
+    def BFS(self):
+        level = [-1] * self.V
+        level[self.source] = 0
+        q = deque([self.source])
+
         while q:
             curV = q.popleft()
-            for nextV in graph[curV]:
-                if parent[nextV] == -1 and capacity[curV][nextV] > 0:
-                    parent[nextV] = curV
-                    if nextV == sink:
-                        isFound = True
-                        break
+            for nextV, cap, _ in self.graph[curV]:
+                if level[nextV] == -1 and cap > 0:
+                    level[nextV] = level[curV] + 1
                     q.append(nextV)
-            if isFound == True:
-                break
-        if parent[sink] == -1:
-            break
+        self.level = level
 
-        totalFlow += 1
-        curV = sink
-        while curV != source:
-            prevV = parent[curV]
-            capacity[prevV][curV] -= 1
-            capacity[curV][prevV] += 1
-            curV = prevV
+        return level[self.sink] != -1
 
-    return totalFlow
+
+    def DFS(self, curV, total):
+        if curV == self.sink:
+            return total
+
+        while self.pointer[curV] < len(self.graph[curV]):
+            i = self.pointer[curV]
+            nextV, cap, revV = self.graph[curV][i]
+
+            if self.level[nextV] == self.level[curV] + 1 and cap > 0:
+                curFlow = self.DFS(nextV, min(total, cap))
+                if curFlow > 0:
+                    self.graph[curV][i][1] -= curFlow
+                    self.graph[nextV][revV][1] += curFlow
+                    return curFlow
+            self.pointer[curV] += 1
+
+        return 0
+
+
+    def maxFlow(self):
+        if self.source == -1 or self.sink == -1:
+            return -1
+
+        result = 0
+        while self.BFS():
+            self.pointer = [0] * self.V
+            while True:
+                curFlow = self.DFS(self.source, INF)
+                if curFlow == 0:
+                    break
+                result += curFlow
+
+        return result
 
 
 def main():
     N, M, K = map(int, input().split())
-    sink = N+M+2
-    graph = [[] for _ in range(sink+1)]
-    capacity = [[0] * (sink+1) for _ in range(sink+1)]
+    dinic = Dinic(N+M+3, 0, N+M+2)
 
     # 초기 그래프 세팅
-    graph[0].append(1)
-    graph[1].append(0)
-    capacity[0][1] = K
+    dinic.addEdge(0, 1, K)
     for i in range(2, N+2):
-        graph[0].append(i)
-        graph[i].append(0)
-        graph[1].append(i)
-        graph[i].append(1)
-        capacity[0][i] = 1
-        capacity[1][i] = 1
+        dinic.addEdge(0, i, 1)
+        dinic.addEdge(1, i, 1)
     for i in range(N+2, N+M+2):
-        graph[sink].append(i)
-        graph[i].append(sink)
-        capacity[i][sink] = 1
+        dinic.addEdge(i, N+M+2, 1)
     for s in range(2, N+2):
         _, *w = list(map(int, input().split()))
         for e in w:
-            e = e+N+1
-            graph[s].append(e)
-            graph[e].append(s)
-            capacity[s][e] = 1
+            dinic.addEdge(s, e+N+1, 1)
 
-    print(EdmondKarp(graph, capacity, 0, sink))
+    print(dinic.maxFlow())
 
 
 main()
