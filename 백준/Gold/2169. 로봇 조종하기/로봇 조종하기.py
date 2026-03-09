6186bff70372337636bde59c6e10bc471ca5a9fd
kh277 <113894741+kh277@users.noreply.github.com>
2026-03-09 10:19:43 +0900

    [Gold II] Title: 로봇 조종하기, Time: 204 ms, Memory: 132336 KB -BaekjoonHub

--- 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.py
+++ 백준/Gold/2169. 로봇 조종하기/로봇 조종하기.py
@@ -1,12 +1,13 @@
 # 백준 2169
 
 import io
+from array import array
 
 input = io.BufferedReader(io.FileIO(0), 1<<18).readline
 
 
 def solve(Y, X, grid):
-    DP = [[0] * X for _ in range(Y)]
+    DP = [array('i', [0]) * X for _ in range(Y)]
 
     # Y=0 처리
     DP[0][0] = grid[0][0]
@@ -16,13 +17,13 @@ def solve(Y, X, grid):
     # 1<=Y<N 처리
     for y in range(1, Y):
         # 왼쪽 -> 오른쪽 처리
-        left = [0] * X
+        left = array('i', [0]) * X
         left[0] = DP[y-1][0] + grid[y][0]
         for x in range(1, X):
             left[x] = max(DP[y-1][x]+grid[y][x], left[x-1]+grid[y][x])
 
         # 오른쪽 -> 왼쪽 처리
-        right = [0] * X
+        right = array('i', [0]) * X
         right[X-1] = DP[y-1][X-1] + grid[y][X-1]
         for x in range(X-2, -1, -1):
             right[x] = max(DP[y-1][x]+grid[y][x], right[x+1]+grid[y][x])
